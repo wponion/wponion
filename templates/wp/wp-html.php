6de6c63d3276cc5447112bc32ebf4d1fc735ccdc
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-08 17:00:03 +0530

    Created Base WP Theme.

--- templates/wp/wp-html.php
+++ templates/wp/wp-html.php
@@ -1,10 +1,22 @@
 <?php
-global $wponion_wp_theme; ?>
+global $wponion_wp_theme;
+$settings = $wponion_wp_theme->settings();
+?>
 <div class="wrap">
-	<div class="wponion-framework-wrap">
-		<div class="wponion-framework">
-			<?php echo $wponion_wp_theme->get_main_menu_html(); ?>
+	<div class="<?php echo $settings->wrap_class( '', true ); ?>">
+		<?php echo $wponion_wp_theme->get_main_menu_html(); ?>
+
+
+		<div id="poststuff">
+			<div class="metabox-holder" id="post-body">
+				<div id="post-body-content">
+					<div class="wponion-content">
+						<div class="wponion-sections">
+							<?php echo $settings->render_inner_html(); ?>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
-
 </div>
--- templates/wp/wp-init.php
+++ templates/wp/wp-init.php
@@ -18,31 +18,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
-if ( ! class_exists( 'WPOnion_WP_Theme' ) ) {
+if ( ! class_exists( 'WPOnion_wp_Theme' ) ) {
 	/**
 	 * Class WPOnion_Theme_WP
 	 *
 	 * @author Varun Sridharan <varunsridharan23@gmail.com>
 	 * @since 1.0
 	 */
-	class WPOnion_WP_Theme extends WPOnion_Theme_Abstract {
+	class WPOnion_wp_Theme extends WPOnion_Theme_Abstract {
 		/**
-		 * WPOnion_Theme_WP constructor.
+		 * WPOnion_wp_Theme constructor.
 		 *
-		 * @param \WPOnion_Settings $settings_instance
-		 * @param string            $theme_file
+		 * @param $plugin_id
 		 */
-		public function __construct( \WPOnion_Settings $settings_instance, string $theme_file = __FILE__ ) {
-			parent::__construct( $settings_instance, $theme_file );
+		public function __construct( $plugin_id ) {
+			parent::__construct( $plugin_id, __FILE__ );
 		}
 
-		/**
-		 *
-		 */
 		public function register_assets() {
 		}
 
-
+		/**
+		 * Generates Main Menu HTML.
+		 *
+		 * @return bool|string
+		 */
 		public function get_main_menu_html() {
 			$return = '<nav class="nav-tab-wrapper">';
 			$menus  = $this->settings()
@@ -82,7 +82,73 @@ if ( ! class_exists( 'WPOnion_WP_Theme' ) ) {
 			$return .= '</nav>';
 			return $return;
 		}
-	}
 
+		/**
+		 * @param string $menu_slug
+		 *
+		 * @return string
+		 */
+		public function submenu_html( $menu_slug = '' ) {
+			$menus = $this->settings()
+				->settings_menus();
+			if ( isset( $menus[ $menu_slug ]['submenu'] ) && ! empty( $menus[ $menu_slug ]['submenu'] ) && is_array( $menus[ $menu_slug ]['submenu'] ) ) {
+				$return = array();
+				foreach ( $menus[ $menu_slug ]['submenu'] as $slug => $menu ) {
+					if ( isset( $menu['is_seperator'] ) && true === $menu['is_seperator'] ) {
+						continue;
+					}
+
+					$attr          = isset( $menu['attributes'] ) ? $menu['attributes'] : array();
+					$attr['class'] = isset( $attr['class'] ) ? $attr['class'] : '';
+					$attr['title'] = isset( $attr['title'] ) ? $attr['title'] : $menu['title'];
+					$icon          = ( ! empty( $menu['icon'] ) ) ? '<i class="' . $menu['icon'] . '"></i>' : '';
+					$page_title    = $menu['title'];
+
+					$attr['class'] .= ( ! empty( $icon ) ) ? ' nav-with-icon ' : '';
+					$attr['class'] .= ( isset( $menu['is_internal_href'] ) && true === $menu['is_internal_href'] ) ? ' nav-internal-href ' : '';
+
+
+					if ( true === $menu['is_active'] ) {
+						$attr['class'] .= ' current ';
+					}
+					$attr['href'] = $menu['href'];
+
+					$attr = wponion_array_to_html_attributes( $attr );
+
+					$return[] = '<li> <a ' . $attr . '>' . $icon . $page_title . '</a>';
+				}
+				$return = implode( '| </li>', $return );
+				$return = '<ul class="wponion-submenus subsubsub"  id="wponion-tab-' . $menus[ $menu_slug ]['name'] . '" >' . $return . '</ul>';
+				return '<h2 class="wponion-subnav-container hndle">' . $return . '</h2>';
+			} else {
+				return '';
+			}
+		}
+
+		/**
+		 * @param $tab_wrap
+		 * @param $options
+		 * @param $is_parent_hidden
+		 *
+		 * @return string
+		 */
+		public function tab_wrap_start( $tab_wrap, $options, $is_parent_hidden ) {
+			return $tab_wrap . '<div class="postbox">' . $this->submenu_html( $options['name'] );
+		}
+
+		/**
+		 * @param $tab_wrap
+		 * @param $options
+		 * @param $is_parent_hidden
+		 *
+		 * @return string
+		 */
+		public function tab_wrap_end( $tab_wrap, $options, $is_parent_hidden ) {
+			return $tab_wrap . '</div>';
+		}
+
+	}
 }
-$wponion_wp_theme = new WPOnion_WP_Theme( $wponion_class );
+
+$wponion_wp_theme = new WPOnion_wp_Theme( $plugin_id );
+return $wponion_wp_theme;
