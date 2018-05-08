311f78d2debe8502b87b1c13e393ea71f1876ada
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-08 14:29:23 +0530

    Added Core Registry Class.

--- core/class-registry.php
+++ core/class-registry.php
@@ -93,3 +93,57 @@ if ( ! class_exists( 'WPOnion_Registry' ) ) {
 		}
 	}
 }
+
+
+if ( ! class_exists( 'WPOnion_Core_Registry' ) ) {
+	/**
+	 * Class WPOnion_Feature_Registry
+	 *
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class WPOnion_Core_Registry implements WPOnion_Registry_Interface {
+		/**
+		 * Stores All Instances
+		 *
+		 * @var array
+		 */
+		protected $registry = array();
+
+		/**
+		 * Adds An Instance To Array.
+		 *
+		 * @param string            $type
+		 * @param \WPOnion_Abstract $instance
+		 *
+		 * @return mixed|void
+		 */
+		public function add( $type = 'settings', \WPOnion_Abstract &$instance ) {
+			if ( ! isset( $this->registry[ $type ] ) ) {
+				$this->registry[ $type ] = array();
+			}
+
+			$key = get_class( $instance );
+
+			if ( ! isset( $registry[ $type ][ $key ] ) ) {
+				$this->registry[ $type ][ $key ] = $instance;
+			}
+		}
+
+		/**
+		 * Returns An Instance.
+		 *
+		 * @param string $type
+		 * @param        $key
+		 *
+		 * @return bool
+		 * @static
+		 */
+		public function get( $type = 'settings', $key ) {
+			if ( isset( $this->registry[ $type ][ $key ] ) ) {
+				return $this->registry[ $type ][ $key ];
+			}
+			return false;
+		}
+	}
+}
