2bce6ce9736228d755327cf8633f10e4246a0600
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-14 13:08:26 +0530

    Updated Fields.js

--- assets/js/wponion-fields.js
+++ assets/js/wponion-fields.js
@@ -1,35 +1,59 @@
-//@codekit-append ./parts/wponion-jquery-noclf-header.js
-//@codekit-append ../assets/js/*.js
-//@codekit-append ./parts/wponion-jquery-noclf-footer.js
-"use strict";
-( function (window, document, $, wponion,wphooks) {
 'use strict';
 
-/**
- * Input Mask JS Handler.
- * @param $elem
- */
-wponion.fields.input_mask = function ($elem) {
-	if (1 >= $elem.length) {
-		$elem.each(function () {
-			var $settings = wponion.field_js_args($(this));
-			if ($settings['inputmask'] !== undefined) {
-				$settings = $settings['inputmask'];
-			}
-			$(this).inputmask($settings);
-		});
-	}
-};
+//@codekit-append ./parts/wponion-jquery-noclf-header.js
+//@codekit-append ./fields/*.js
+//@codekit-append ./parts/wponion-jquery-noclf-footer.js
+(function (window, document, $, wponion, wphooks) {
+	/**
+  * Input Mask JS Handler.
+  * @param $elem
+  */
+	wponion.fields.input_mask = function ($elem) {
+		if (1 >= $elem.length) {
+			$elem.each(function () {
+				var $settings = wponion.field_js_args($(this));
+				if ($settings['inputmask'] !== undefined) {
+					$(this).inputmask($settings['inputmask']);
+				}
+			});
+		}
+	};
 
-/**
- * Hooked To Auto Reload Fields.
- */
-wphooks.addAction('wponion_reload_fields', function () {
-	wponion.fields.input_mask(wponion_elem().find(".wponion-inputmask input[data-wponion-inputmask]"));
-});
+	/**
+  * Hooked To Auto Reload Fields.
+  */
+	wphooks.addAction('wponion_reload_fields', function () {
+		wponion.fields.input_mask(wponion_elem().find("input[data-wponion-inputmask]"));
+	});
+	wponion.fields.max_length = function ($elem) {
+		if ($elem.length > 0) {
+			$elem.each(function () {
+				var $settings = wponion.field_js_args($(this), {});
+				if ($settings['max_length'] !== undefined) {
+					$settings = $settings['max_length'];
+					$settings['appendToParent'] = true;
 
+					if ($settings['warningClass'] === undefined) {
+						$settings['warningClass'] = 'badge badge-success';
+					}
 
-})
-( window, document, jQuery, wponion, wp.hooks );
+					if ($settings['limitReachedClass'] === undefined) {
+						$settings['limitReachedClass'] = 'badge badge-danger';
+					}
 
+					$(this).maxlength($settings);
+				}
+
+				console.log(wponion.field_js_args($(this), {}));
+			});
+		}
+	};
+
+	/**
+  * Hooked To Auto Reload Fields.
+  */
+	wphooks.addAction('wponion_reload_fields', function () {
+		wponion.fields.max_length(wponion_elem().find("[data-wponion-maxlength]"));
+	});
+})(window, document, jQuery, wponion, wp.hooks);
 //# sourceMappingURL=wponion-fields.js.map
