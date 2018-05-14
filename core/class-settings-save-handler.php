c61ddda4c18542ddfea1740678b104733afe0e3c
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-14 15:48:08 +0530

    Created Settings Save Handler

---
+++ core/class-settings-save-handler.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * Initial version created 14-05-2018 / 03:43 PM
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * @package
+ * @link
+ * @copyright 2018 Varun Sridharan
+ * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+if ( ! class_exists( 'WPOnion_Settings_Save_Handler' ) ) {
+	class WPOnion_Settings_Save_Handler extends WPOnion_Save_Handler {
+		public function run() {
+			var_dump( $_REQUEST );
+		}
+	}
+}
