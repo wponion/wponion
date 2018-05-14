05c7bfbdd4e0cd9732c3798c154d8ed1d7922df8
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-14 13:24:15 +0530

    Updated Paths

--- src/js/wponion-plugins.js
+++ src/js/wponion-plugins.js
@@ -8,17 +8,17 @@ var wponion = {
 	themes: {}
 };
 
-//@codekit-append ../vendors/node_modules/jquery.actual/jquery.actual.js
+//@codekit-append ../../node_modules/jquery.actual/jquery.actual.js
 //@codekit-append ../vendors/jquery-interdependencies/jquery-interdependencies.js
 //@codekit-append ../vendors/wp-js-hooks.js
-//@codekit-append ../vendors/node_modules/popper.js/dist/umd/popper.js
+//@codekit-append ../../node_modules/popper.js/dist/umd/popper.js
 
 //@codekit-append ./parts/wponion-jquery-noclf-header.js
-//@codekit-append ../vendors/node_modules/bootstrap/js/dist/index.js
-//@codekit-append ../vendors/node_modules/bootstrap/js/dist/util.js
-//@codekit-append ../vendors/node_modules/bootstrap/js/dist/tooltip.js
-//@codekit-append ../vendors/node_modules/bootstrap/js/dist/popover.js
-//@codekit-append ../vendors/node_modules/bootstrap/js/dist/button.js
-//@codekit-append ../vendors/node_modules/bootstrap/js/dist/dropdown.js
-//@codekit-append ../vendors/node_modules/bootstrap-maxlength/src/bootstrap-maxlength.js
+//@codekit-append ../../node_modules/bootstrap/js/dist/index.js
+//@codekit-append ../../node_modules/bootstrap/js/dist/util.js
+//@codekit-append ../../node_modules/bootstrap/js/dist/tooltip.js
+//@codekit-append ../../node_modules/bootstrap/js/dist/popover.js
+//@codekit-append ../../node_modules/bootstrap/js/dist/button.js
+//@codekit-append ../../node_modules/bootstrap/js/dist/dropdown.js
+//@codekit-append ../../node_modules/bootstrap-maxlength/src/bootstrap-maxlength.js
 //@codekit-append ./parts/wponion-jquery-noclf-footer.js
