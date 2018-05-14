341ae3fd6ebeb803972edd7c02b1b3b74ed2af5a
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-14 15:13:16 +0530

    Created DB Save Handler.

---
+++ core/class-save-handler.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ * Initial version created 14-05-2018 / 03:10 PM
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * @package
+ * @link
+ * @copyright 2018 Varun Sridharan
+ * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+if ( ! class_exists( 'WPOnion_Save_Handler' ) ) {
+	/**
+	 * Class WPOnion_Save_Handler
+	 *
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class WPOnion_Save_Handler extends WPOnion_Abstract {
+		/**
+		 * WPOnion_Save_Handler constructor.
+		 */
+		public function __construct() {
+		}
+	}
+}
