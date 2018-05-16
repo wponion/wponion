6010f0090edb398701dcc7ca0b3a5f8f8be327d0
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-16 17:19:15 +0530

    * Rearranged Few Functions * Added SettingsArgs * Added GloablDebug Info view *

--- src/js/wponion-core.js
+++ src/js/wponion-core.js
@@ -16,7 +16,181 @@ let wponion_elem = function () {
 	 */
 	wponion.elem = $( '.wponion-framework' );
 
+	/**
+	 * Stores Global Debug Info.
+	 * @type {null}
+	 */
+	wponion.debug_info = null;
+
+	/**
+	 * Stores Global Settings args.
+	 * @type {null}
+	 */
 	wponion.settings_args = null;
+
+	/**
+	 * Stores Global Translation.
+	 * @type {null}
+	 */
+	wponion.text = null;
+
+	//Core Functions
+
+	/**
+	 * Checks for Document / Window for the elemnts JS settings. if exists it returns or default will be returned.
+	 * @type {function(*, *)}
+	 */
+	wponion.field_js_args = ( ($elem, $default) => {
+		return wponion.window_vars( wponion.field_js_id( $elem ), $default );
+	} );
+
+	/**
+	 * Returns Fields JS ID.
+	 * @type {function(*)}
+	 */
+	wponion.field_js_id = ( ($elem) => {
+		return $elem.attr( "data-wponion-jsid" );
+	} );
+
+	/**
+	 * Checks And Returns Variable From Window.
+	 * @type {function(*=, *)}
+	 */
+	wponion.window_vars = ( ($var_id, $default) => {
+		if ( $var_id ) {
+			if ( typeof window[ $var_id ] === 'undefined' || window[ $var_id ] === undefined ) {
+				return $default;
+			}
+			return window[ $var_id ];
+		}
+
+		return $default;
+	} );
+
+	/**
+	 * Returns URL Query String as object
+	 * @example http://example.com?q1=22&q2=49 turns into {q1:22,q2:49}
+	 * @param str
+	 * @returns {any}
+	 */
+	wponion.url_param = function (str) {
+		return ( str || document.location.search ).replace( /(^\?)/, '' ).split( "&" ).map( function (n) {
+			return n = n.split( "=" ), this[ n[ 0 ] ] = n[ 1 ], this
+		}.bind( {} ) )[ 0 ];
+	};
+
+	/**
+	 * Stores / Retrives Display Text
+	 * @type {function(*, *)}
+	 * @private
+	 */
+	wponion.__ = ( ($key, $default) => {
+		return ( wponion.text[ $key ] !== undefined ) ? wponion.text[ $key ] : $default;
+	} );
+
+	//Gloabl Functions / Fields
+
+	/**
+	 * Handles WPOnion Settings Page Loading Screen.
+	 * @type {function()}
+	 */
+	wponion.handle_loading_screen = ( () => {
+		if ( wponion_elem().find( '.wponion-page-loader' ).length > 0 ) {
+			wponion_elem().find( '.wponion-page-loader' ).fadeOut( 'slow', function () {
+				$( this ).hide();
+			} );
+
+			window.onbeforeunload = function () {
+				wponion_elem().find( '.wponion-page-loader' ).show();
+				//return 'Are you sure';
+			};
+		}
+	} );
+
+	/**
+	 * Inits Field Debug Info.
+	 * @type {function()}
+	 */
+	wponion.init_field_debug = ( () => {
+		if ( wponion_elem().find( '.wponion-field-debug' ).length > 0 ) {
+			wponion_elem().find( '.wponion-field-debug a.wponion-field-debug-handle' ).on( 'click', function () {
+				let $data = wponion.field_js_args( $( this ), {} );
+				if ( $data[ 'debug_info' ] !== undefined ) {
+					let $divID = wponion.field_js_id( $( this ) ) + 'debugINFO',
+						$notice_txt = "<p class='wponion-field-debug-notice'>" + wponion.settings( 'debug_notice' ) + "</p>",
+						$elem = $( "<div id='" + $divID + "' class='wponion-field-debug-popup' ><div id='" + $divID + "' ></div> " + $notice_txt + "</div>" );
+
+					let $ops = swal( {
+						html: $elem,
+						showConfirmButton: true,
+						confirmButtonText: wponion.__( 'get_json_output', 'Get JSON Output' ),
+						showCloseButton: false,
+						width: '500px',
+						onOpen: ( () => {
+							$( '#swal2-content #' + $divID ).JSONView( $data[ 'debug_info' ], {
+								collapsed: true,
+								recursive_collapser: true,
+							} );
+						} )
+					} ).then( (result) => {
+						if ( result.value ) {
+							swal( {
+								width: '600px',
+								html: '<textarea style="min-width:550px; min-height:300px">' + JSON.stringify( wponion.debug_info ) + '</textarea>'
+
+							} )
+						}
+					} );
+
+				}
+			} )
+		}
+	} );
+
+	/**
+	 * Renders Gloabl Debug Info.
+	 * @type {function()}
+	 */
+	wponion.init_debug_popup = ( () => {
+		let $ahandle = $( 'a.wponion-global-debug-handle' );
+		if ( wponion.debug_info === null ) {
+			if ( $ahandle.length > 0 ) {
+				let $defined_vars = wponion.window_vars( 'wponion_defined_vars' ),
+					$json = {};
+				$.each( $defined_vars, ( ($i, $el) => {
+					$json[ $el ] = wponion.window_vars( $el );
+				} ) );
+				wponion.debug_info = $json;
+			}
+		}
+
+		$ahandle.on( 'click', function (e) {
+			e.preventDefault();
+			let $ops = swal( {
+				title: wponion.__( 'global_json_output', "Global WPOnion Debug Data" ),
+				html: $( "#wponiondebuginfopopup > div" ),
+				showConfirmButton: true,
+				confirmButtonText: wponion.__( 'get_json_output', 'Get JSON Output' ),
+				showCloseButton: false,
+				width: '800px',
+				onOpen: ( () => {
+					$( '#swal2-content #wponion-global-debug-content' ).JSONView( wponion.debug_info, {
+						collapsed: true,
+						recursive_collapser: true,
+					} );
+				} )
+			} ).then( (result) => {
+				if ( result.value ) {
+					swal( {
+						width: '600px',
+						html: '<textarea style="min-width:550px; min-height:300px">' + JSON.stringify( wponion.debug_info ) + '</textarea>'
+
+					} )
+				}
+			} );
+		} );
+
+	} );
 
 	/**
 	 * Handles Framework ToolTip Functions.
@@ -55,6 +229,10 @@ let wponion_elem = function () {
 		}
 	} );
 
+	/**
+	 * Retrives Option values from wponion.settings
+	 * @type {function(*, *)}
+	 */
 	wponion.settings = ( ($key, $default) => {
 		if ( wponion.settings_args[ $key ] !== undefined ) {
 			return wponion.settings_args[ $key ];
@@ -132,70 +310,24 @@ let wponion_elem = function () {
 		wponion.dependency();
 		wponion.tooltip( wponion.elem.find( ' .wponion-help' ) );
 		wponion.tooltip_field( wponion.elem.find( '.wponion-field-tooltip' ) );
+		wponion.init_field_debug();
+		wponion.init_debug_popup();
 		wphooks.doAction( "wponion_reload_fields" );
-	} );
-
-	/**
-	 * Checks for Document / Window for the elemnts JS settings. if exists it returns or default will be returned.
-	 * @type {function(*, *)}
-	 */
-	wponion.field_js_args = ( ($elem, $default) => {
-		let $js_id = $elem.attr( "data-wponion-jsid" );
-		return wponion.window_vars( $js_id, $default );
-	} );
-
-	/**
-	 * Checks And Returns Variable From Window.
-	 * @type {function(*=, *)}
-	 */
-	wponion.window_vars = ( ($var_id, $default) => {
-		if ( $var_id ) {
-			if ( typeof window[ $var_id ] === 'undefined' || window[ $var_id ] === undefined ) {
-				return $default;
-			}
-			return window[ $var_id ];
-		}
-
-		return $default;
-	} );
-
-	/**
-	 * Returns URL Query String as object
-	 * @example http://example.com?q1=22&q2=49 turns into {q1:22,q2:49}
-	 * @param str
-	 * @returns {any}
-	 */
-	wponion.url_param = function (str) {
-		return ( str || document.location.search ).replace( /(^\?)/, '' ).split( "&" ).map( function (n) {
-			return n = n.split( "=" ), this[ n[ 0 ] ] = n[ 1 ], this
-		}.bind( {} ) )[ 0 ];
-	};
-
-	/**
-	 * Handles WPOnion Settings Page Loading Screen.
-	 * @type {function()}
-	 */
-	wponion.handle_loading_screen = ( () => {
-		if ( wponion_elem().hasClass( 'wponion-settings-module' ) ) {
-			if ( wponion_elem().find( '.wponion-page-loader' ).length > 0 ) {
-				wponion_elem().find( '.wponion-page-loader' ).fadeOut( 'slow', function () {
-					$( this ).hide();
-				} )
-			}
-
-			window.onbeforeunload = function () {
-				wponion_elem().find( '.wponion-page-loader' ).show();
-			};
-		}
-
 	} );
 
 	/**
 	 * Below Code Runs On Window.load
 	 */
 	$( window ).on( "load", ( () => {
+		/**
+		 * Retrives Basic Varaibles.
+		 */
 		wponion.settings_args = wponion.window_vars( 'wponion_core', {} );
+		wponion.text = wponion.window_vars( 'wponion_i18n', {} );
+
+		/** Inits All Fields. **/
 		wponion.reload();
+
 		wponion.handle_loading_screen();
 		wphooks.doAction( 'wponion_init' );
 	} ) );
