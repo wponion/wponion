b4c5ca13a96f0de75f877f6a7fc4dc465125cf66
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-17 17:38:47 +0530

    Totally Recoded

--- src/js/wponion-wp-theme.js
+++ src/js/wponion-wp-theme.js
@@ -1,65 +1,79 @@
-( (window, document, $, wp, wponion, wphooks) => {
-	wponion.themes.wp = {
-		submenu_single_page: ( () => {
-			wponion_elem().find( '.wponion-submenus.subsubsub a' ).on( 'click', function (e) {
-				e.preventDefault();
-				let $href = $( this ).attr( 'href' );
-				$href = wponion.url_param( $href );
+/**
+ * @param window = Window Object
+ * @param document = Document Object
+ * @param $ = jQuery Object
+ * @param wpo = $wponion object
+ * @param wpf = $wonion.field object
+ * @param wpt = $wponion.theme object.
+ */
+( ( window, document, $, wpo, wp ) => {
+	let $wpt    = wpo._theme();
+	let wpt     = $wpt.fn;
+	let wphooks = wp.hooks;
 
-				if ( $href[ 'section-id' ] !== undefined && $href[ 'parent-id' ] !== undefined ) {
-					let $parent = 'wponion-tab-' + $href[ 'parent-id' ];
-					let $section = $parent + '-' + $href[ 'section-id' ];
-					let $all_actives = wponion_elem().find( 'div#' + $parent + ' div.wponion-section-wraps' );
-					let $current = wponion_elem().find( 'div#' + $parent + ' div#' + $section );
+	/**
+	 * Handles Single Submenu
+	 */
+	wpt.WP_submenu = function () {
+		let $this = this;
+		$this.elem.find( '.wponion-submenus.subsubsub a' ).on( 'click', function ( e ) {
+			e.preventDefault();
+			let $href = $( this ).attr( 'href' );
+			$href     = wpo.url_to_object( $href );
 
-					$all_actives.hide();
-					$current.show();
-					$( this ).parent().parent().find( 'a.current' ).removeClass( 'current' );
-					$( this ).addClass( 'current' );
-				} else {
-					window.location.href = $( this ).attr( 'href' );
-				}
-			} )
-		} ),
-		mainmenu_single_page: ( () => {
-			wponion_elem().find( 'nav.nav-tab-wrapper a' ).on( 'click', function (e) {
-				e.preventDefault();
+			if ( $href[ 'section-id' ] !== undefined && $href[ 'parent-id' ] !== undefined ) {
+				let $parent      = 'wponion-tab-' + $href[ 'parent-id' ];
+				let $section     = $parent + '-' + $href[ 'section-id' ];
+				let $all_actives = $this.elem.find( 'div#' + $parent + ' div.wponion-section-wraps' );
+				let $current     = $this.elem.find( 'div#' + $parent + ' div#' + $section );
 
-				let $href = $( this ).attr( 'href' );
-				$href = wponion.url_param( $href );
-
-				if ( $href[ 'parent-id' ] !== undefined ) {
-					let $parent = 'wponion-tab-' + $href[ 'parent-id' ];
-					let $all_actives = wponion_elem().find( ' div.wponion-parent-wraps' );
-					let $current = wponion_elem().find( 'div#' + $parent );
-
-					$all_actives.hide();
-					$current.show();
-					$( this ).parent().find( 'a.nav-tab-active ' ).removeClass( 'nav-tab-active ' );
-					$( this ).addClass( 'nav-tab-active ' );
-				} else {
-					window.location.href = $( this ).attr( 'href' );
-				}
-			} )
-		} )
-
+				$all_actives.hide();
+				$current.show();
+				$( this ).parent().parent().find( 'a.current' ).removeClass( 'current' );
+				$( this ).addClass( 'current' );
+			} else {
+				window.location.href = $( this ).attr( 'href' );
+			}
+		} );
+		return $this;
 	};
 
-	wphooks.addAction( 'wponion_loaded', ( () => {
-		$( window ).on( 'load', function () {
+	/**
+	 * Handles Main Menu.
+	 * @returns {$wpt}
+	 */
+	wpt.WP_main_menu = function () {
+		let $this = this;
+		$this.elem.find( 'nav.nav-tab-wrapper a' ).on( 'click', function ( e ) {
+			e.preventDefault();
 
-		} );
+			let $href = $( this ).attr( 'href' );
+			$href     = wpo.url_to_object( $href );
 
-		$( document ).on( 'ready', function () {
-			if ( wponion_elem().hasClass( 'wponion-submenu-single-page' ) ) {
-				wponion.themes.wp.submenu_single_page();
+			if ( $href[ 'parent-id' ] !== undefined ) {
+				let $parent      = 'wponion-tab-' + $href[ 'parent-id' ];
+				let $all_actives = $this.elem.find( ' div.wponion-parent-wraps' );
+				let $current     = $this.elem.find( 'div#' + $parent );
+				$all_actives.hide();
+				$current.show();
+				$( this ).parent().find( 'a.nav-tab-active ' ).removeClass( 'nav-tab-active ' );
+				$( this ).addClass( 'nav-tab-active ' );
+			} else {
+				window.location.href = $( this ).attr( 'href' );
 			}
+		} )
+		return $this;
+	};
 
-			if ( wponion_elem().hasClass( 'wponion-single-page' ) ) {
-				wponion.themes.wp.mainmenu_single_page();
-				wponion.themes.wp.submenu_single_page();
-			}
-		} );
+	wphooks.addAction( 'wponion_before_init', ( () => {
+		var $elem = $( '.wponion-framework' );
+		if ( $elem.hasClass( 'wponion-submenu-single-page' ) ) {
+			$wpt( '.wponion-framework' ).WP_submenu();
+		}
+
+		if ( $elem.hasClass( 'wponion-single-page' ) ) {
+			$wpt( '.wponion-framework' ).WP_main_menu().WP_submenu();
+		}
 	} ) );
 
-} )( window, document, jQuery, wp, wponion, wp.hooks );
+} )( window, document, jQuery, $wponion, wp );
