bd889239c20718961ead0fda636a05dc02ed6e1b
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-17 17:36:28 +0530

    Totally Recoded

--- assets/js/wponion-fields.js
+++ assets/js/wponion-fields.js
@@ -1,37 +1,49 @@
-'use strict';
+"use strict";
 
-//@codekit-append ./parts/wponion-jquery-noclf-header.js
-//@codekit-append ./fields/*.js
-//@codekit-append ./parts/wponion-jquery-noclf-footer.js
-(function (window, document, $, wponion, wphooks) {
+/**
+ * @param window = Window Object
+ * @param document = Document Object
+ * @param $ = jQuery Object
+ * @param wpo = $wponion object
+ * @param wpf = $wonion.field object
+ * @param wpt = $wponion.theme object.
+ */
+(function (window, document, $, wpo, wp) {
+	var $wpf = wpo._field();
+	var wpf = $wpf.fn;
+	var wphooks = wp.hooks;
+
 	/**
   * Input Mask JS Handler.
-  * @param $elem
   */
-	wponion.fields.input_mask = function ($elem) {
-		if ($elem.length > 0) {
-			$elem.each(function () {
-				var $settings = wponion.field_js_args($(this));
+	wpf.inputmask = function () {
+		if (this.elem.find("input[data-wponion-inputmask]").length > 0) {
+			this.elem.find("input[data-wponion-inputmask]").each(function () {
+				var $settings = wpo.field_args($(this));
 				if ($settings['inputmask'] !== undefined) {
 					$(this).inputmask($settings['inputmask']);
+					wpo.__plugin_debug_info($(this), 'inputmask', $settings['inputmask']);
 				}
 			});
 		}
+		return wpo;
 	};
 
 	/**
-  * Hooked To Auto Reload Fields.
+  * Handles Maxlength Field.
   */
-	wphooks.addAction('wponion_reload_fields', function () {
-		wponion.fields.input_mask(wponion_elem().find("input[data-wponion-inputmask]"));
-	});
-	wponion.fields.max_length = function ($elem) {
-		if ($elem.length > 0) {
-			$elem.each(function () {
-				var $settings = wponion.field_js_args($(this), {});
+	wpf.maxlength = function () {
+		if (this.elem.find("[data-wponion-maxlength]").length > 0) {
+			this.elem.find("[data-wponion-maxlength]").each(function () {
+				var $settings = wpo.field_args($(this), {});
 				if ($settings['max_length'] !== undefined) {
+
 					$settings = $settings['max_length'];
 					$settings['appendToParent'] = true;
+
+					if ($settings['threshold'] !== undefined) {
+						$settings['threshold'] = parseInt($settings['threshold']);
+					}
 
 					if ($settings['warningClass'] === undefined) {
 						$settings['warningClass'] = 'badge badge-success';
@@ -42,18 +54,64 @@
 					}
 
 					$(this).maxlength($settings);
+					wpo.__plugin_debug_info($(this), 'max_length', $settings);
 				}
-
-				console.log(wponion.field_js_args($(this), {}));
 			});
 		}
+		return wpo;
 	};
 
 	/**
-  * Hooked To Auto Reload Fields.
+  * Renders Fields Debug Popup.
   */
-	wphooks.addAction('wponion_reload_fields', function () {
-		wponion.fields.max_length(wponion_elem().find("[data-wponion-maxlength]"));
+	wpf.field_debug = function () {
+		if (this.elem.find('.wponion-field-debug').length > 0) {
+			var $elem = this.elem;
+
+			$elem.find('.wponion-field-debug a.wponion-field-debug-handle').on('click', function () {
+				var $data = wpo._get_debug_info($(this), {});
+				var $divID = wpo.field_js_id($(this)) + 'debugINFO',
+				    $notice_txt = "<p class='wponion-field-debug-notice'>" + wpo.settings('debug_notice') + "</p>",
+				    $elem = $("<div id='" + $divID + "' class='wponion-field-debug-popup' ><div id='" + $divID + "' ></div> " + $notice_txt + "</div>");
+
+				var $ops = swal({
+					html: $elem,
+					showConfirmButton: true,
+					confirmButtonText: wpo.txt('get_json_output', 'Get JSON Output'),
+					showCloseButton: false,
+					width: '800px',
+					onOpen: function onOpen() {
+						$('#swal2-content > div > #' + $divID).jsonView($data).overlayScrollbars({
+							className: "os-theme-dark",
+							resize: "vertical"
+						});
+					}
+				}).then(function (result) {
+					if (result.value) {
+						swal({
+							width: '600px',
+							html: '<textarea style="min-width:550px; min-height:300px">' + JSON.stringify($data) + '</textarea>'
+						});
+					}
+				});
+			});
+		}
+		return wpo;
+	};
+
+	/**
+  * Reloads All Fields Instance. For the given key.
+  */
+	wpf.reload = function () {
+		wphooks.addAction('wponion_before_fields_reload');
+		this.inputmask().maxlength().field_debug();
+		wphooks.addAction('wponion_after_fields_reload');
+	};
+
+	wphooks.addAction('wponion_before_init', function () {
+		console.time("O");
+		$wpf('.wponion-framework').reload();
+		console.timeEnd("O");
 	});
-})(window, document, jQuery, wponion, wp.hooks);
+})(window, document, jQuery, $wponion, wp);
 //# sourceMappingURL=wponion-fields.js.map
--- src/js/wponion-fields.js
+++ src/js/wponion-fields.js
@@ -1,3 +1,116 @@
-//@codekit-append ./parts/wponion-jquery-noclf-header.js
-//@codekit-append ./fields/*.js
-//@codekit-append ./parts/wponion-jquery-noclf-footer.js
+/**
+ * @param window = Window Object
+ * @param document = Document Object
+ * @param $ = jQuery Object
+ * @param wpo = $wponion object
+ * @param wpf = $wonion.field object
+ * @param wpt = $wponion.theme object.
+ */
+( ( window, document, $, wpo, wp ) => {
+	let $wpf    = wpo._field();
+	let wpf     = $wpf.fn;
+	let wphooks = wp.hooks;
+
+	/**
+	 * Input Mask JS Handler.
+	 */
+	wpf.inputmask = function () {
+		if ( this.elem.find( "input[data-wponion-inputmask]" ).length > 0 ) {
+			this.elem.find( "input[data-wponion-inputmask]" ).each( function () {
+				let $settings = wpo.field_args( $( this ) );
+				if ( $settings[ 'inputmask' ] !== undefined ) {
+					$( this ).inputmask( $settings[ 'inputmask' ] );
+					wpo.__plugin_debug_info( $( this ), 'inputmask', $settings[ 'inputmask' ] );
+				}
+			} )
+		}
+		return wpo;
+	};
+
+	/**
+	 * Handles Maxlength Field.
+	 */
+	wpf.maxlength = function () {
+		if ( this.elem.find( "[data-wponion-maxlength]" ).length > 0 ) {
+			this.elem.find( "[data-wponion-maxlength]" ).each( function () {
+				let $settings = wpo.field_args( $( this ), {} );
+				if ( $settings[ 'max_length' ] !== undefined ) {
+
+					$settings                     = $settings[ 'max_length' ];
+					$settings[ 'appendToParent' ] = true;
+
+					if ( $settings[ 'threshold' ] !== undefined ) {
+						$settings[ 'threshold' ] = parseInt( $settings[ 'threshold' ] );
+					}
+
+					if ( $settings[ 'warningClass' ] === undefined ) {
+						$settings[ 'warningClass' ] = 'badge badge-success';
+					}
+
+					if ( $settings[ 'limitReachedClass' ] === undefined ) {
+						$settings[ 'limitReachedClass' ] = 'badge badge-danger';
+					}
+
+					$( this ).maxlength( $settings );
+					wpo.__plugin_debug_info( $( this ), 'max_length', $settings );
+				}
+			} )
+		}
+		return wpo;
+	};
+
+
+	/**
+	 * Renders Fields Debug Popup.
+	 */
+	wpf.field_debug = function () {
+		if ( this.elem.find( '.wponion-field-debug' ).length > 0 ) {
+			let $elem = this.elem;
+
+			$elem.find( '.wponion-field-debug a.wponion-field-debug-handle' ).on( 'click', function () {
+				let $data       = wpo._get_debug_info( $( this ), {} );
+				let $divID      = wpo.field_js_id( $( this ) ) + 'debugINFO',
+					$notice_txt = "<p class='wponion-field-debug-notice'>" + wpo.settings( 'debug_notice' ) + "</p>",
+					$elem       = $( "<div id='" + $divID + "' class='wponion-field-debug-popup' ><div id='" + $divID + "' ></div> " + $notice_txt + "</div>" );
+
+				let $ops = swal( {
+					html: $elem,
+					showConfirmButton: true,
+					confirmButtonText: wpo.txt( 'get_json_output', 'Get JSON Output' ),
+					showCloseButton: false,
+					width: '800px',
+					onOpen: ( () => {
+						$( '#swal2-content > div > #' + $divID ).jsonView( $data ).overlayScrollbars( {
+							className: "os-theme-dark",
+							resize: "vertical",
+						} );
+					} )
+				} ).then( ( result ) => {
+					if ( result.value ) {
+						swal( {
+							width: '600px',
+							html: '<textarea style="min-width:550px; min-height:300px">' + JSON.stringify( $data ) + '</textarea>'
+						} )
+					}
+				} );
+			} )
+		}
+		return wpo;
+	};
+
+	/**
+	 * Reloads All Fields Instance. For the given key.
+	 */
+	wpf.reload = function () {
+		wphooks.addAction( 'wponion_before_fields_reload' );
+		this.inputmask().maxlength().field_debug();
+		wphooks.addAction( 'wponion_after_fields_reload' );
+	};
+
+	wphooks.addAction( 'wponion_before_init', ( () => {
+		console.time( "O" );
+		$wpf( '.wponion-framework' ).reload();
+		console.timeEnd( "O" );
+	} ) );
+
+} )( window, document, jQuery, $wponion, wp );
