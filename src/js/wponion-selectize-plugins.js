6546fc71fadd5eb6c887a68553b51bff160c7323
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-19 06:59:48 +0530

    Updated Selectize Framework

---
+++ src/js/wponion-selectize-plugins.js
@@ -0,0 +1,3 @@
+//@codekit-append ../../node_modules/selectize.js/dist/js/standalone/selectize.js
+// @codekit-append ../vendors/selectize-themes/click_edit.js
+//@codekit-append ../vendors/selectize-themes/condensed_dropdown.js
