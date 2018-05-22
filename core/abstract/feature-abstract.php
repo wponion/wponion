c288cf9f8166472291c20e2c751ae85027bbfd32
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-22 17:23:20 +0530

    added builtin theme handler

--- core/abstract/feature-abstract.php
+++ core/abstract/feature-abstract.php
@@ -25,6 +25,14 @@ if ( ! class_exists( 'WPOnion_Feature_Abstract' ) ) {
 	 */
 	abstract class WPOnion_Feature_Abstract extends WPOnion_Module_Field_Handler {
 		/**
+		 * Stores Current template information.
+		 * current_theme
+		 *
+		 * @var bool
+		 */
+		protected $current_theme = false;
+
+		/**
 		 * Stores Fields MD5.
 		 *
 		 * @var bool
@@ -149,6 +157,52 @@ if ( ! class_exists( 'WPOnion_Feature_Abstract' ) ) {
 <a  title="' . __( 'WPOnion Debug POPUP' ) . '" href="?loaddebug=true" class="wponion-global-debug-handle thickbox">' . wponion_icon( 'fas fa-info-circle' ) . '</a>	';
 			}
 			return '';
+		}
+
+		/**
+		 * inits selected theme.
+		 *
+		 * @param string $theme_init
+		 * @param string $theme_html
+		 */
+		protected function init_theme( $theme_init = '-init.php', $theme_html = '-settings-html.php' ) {
+			if ( false === $this->current_theme ) {
+				$theme          = $this->option( 'theme' );
+				$template_path  = $this->option( 'template_path' );
+				$file           = wponion_locate_template( $theme . '/' . $theme . $theme_init, $template_path );
+				$html_file      = wponion_locate_template( $theme . '/' . $theme . $theme_html, $template_path );
+				$callback_class = 'WPOnion_' . strtolower( $theme ) . '_Theme';
+
+				if ( file_exists( $file ) && file_exists( $html_file ) ) {
+					$this->current_theme = array(
+						'class'     => $callback_class,
+						'success'   => true,
+						'file'      => $file,
+						'html_file' => wponion_locate_template( $theme . '/' . $theme . $theme_html, $template_path ),
+					);
+					wponion_get_template( $theme . '/' . $theme . $theme_init, array( 'plugin_id' => $this->plugin_id() ) );
+				} else {
+					$this->current_theme = array(
+						'success' => false,
+						'html'    => '<h3>' . __( 'Theme Not Found :-( ' ) . '</h3>',
+					);
+				}
+			} else {
+				if ( false === $this->current_theme['success'] ) {
+					echo $this->current_theme['html'];
+				} else {
+					include $this->current_theme['html_file'];
+				}
+			}
+		}
+
+		/**
+		 * Returns Current Theme's instance.
+		 *
+		 * @return bool
+		 */
+		protected function theme_instance() {
+			return wponion_core_registry( $this->current_theme['class'] );
 		}
 	}
 }
