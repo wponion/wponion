58244dbaa740302b1e60f27348790d5e038ee247
Varun Sridharan <varunsridharan23@gmail.com>
2018-05-23 06:21:16 +0530

    bug fixed with icon file json

--- core/helpers/assets.php
+++ core/helpers/assets.php
@@ -116,8 +116,8 @@ if ( ! function_exists( 'wponion_icon_libraries' ) ) {
 	function wponion_icon_libraries() {
 		return apply_filters( 'wponion_icon_libraries', array(
 			__( 'DashIcons' )   => WPONION_PATH . 'assets/json/dashicons.json',
-			__( 'FontAwesome' ) => WPONION_PATH . 'assets/json/fontawesome.json',
-			__( 'TypIcons' )    => WPONION_PATH . 'assets/json/typicons.json',
+			__( 'FontAwesome' ) => WPONION_PATH . 'assets/json/fontawesome_icons.json',
+			__( 'TypIcons' )    => WPONION_PATH . 'assets/json/typicons_icons.json',
 		) );
 	}
 }
