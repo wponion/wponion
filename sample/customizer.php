98f13b45b8bd2aa9fe0f7fe1cc07efca49d8b670
Varun Sridharan <varunsridharan23@gmail.com>
2018-06-28 12:45:58 +0530

    Updated Customizer Demo Source.

--- sample/customizer.php
+++ sample/customizer.php
@@ -12,6 +12,16 @@
  * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
+$customizer_fields = array();
+
+foreach ( $wpof as $name => $field ) {
+	$customizer_fields[] = array(
+		'name'   => 'wponion_' . $name,
+		'title'  => $name . ' Field',
+		'fields' => $field->get(),
+	);
+}
+
 $ins = new \WPOnion\Modules\customizer( array(
 	'option_name' => '_wponion_customizer_demo',
 	'plugin_id'   => 'wponion_customizer',
@@ -19,37 +29,7 @@ $ins = new \WPOnion\Modules\customizer( array(
 	array(
 		'name'     => 'wponion',
 		'title'    => 'WPOnion Fields',
-		'sections' => array(
-			array( 'name' => 'wponion_text', 'title' => 'text Field', 'fields' => $wponion_text_options->get() ),
-			array( 'name' => 'wponion_textarea', 'title' => 'Textarea', 'fields' => $wponion_textarea->get() ),
-			array( 'name' => 'wponion_checkbox', 'title' => 'Checkbox', 'fields' => $wponion_checkbox->get() ),
-			array( 'name' => 'wponion_radio', 'title' => 'Radio', 'fields' => $wponion_radio->get() ),
-			array(
-				'name'   => 'wponion_color_palette',
-				'title'  => 'Color Palette',
-				'fields' => $wponion_color_palette->get(),
-			),
-			array(
-				'name'   => 'wponion_color_picker',
-				'title'  => 'Color Picker',
-				'fields' => $wponion_color_picker->get(),
-			),
-			array(
-				'name'   => 'wponion_font_picker',
-				'title'  => 'Font Picker',
-				'fields' => $wponion_font_picker->get(),
-			),
-			array(
-				'name'   => 'wponion_image_picker',
-				'title'  => 'WP Images',
-				'fields' => $wponion_images->get(),
-			),
-			array(
-				'name'   => 'wponion_keyvalue',
-				'title'  => 'Key Value',
-				'fields' => $wponion_keyval,
-			),
-		),
+		'sections' => $customizer_fields,
 	),
 ) );
 
