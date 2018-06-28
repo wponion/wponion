d1bedee4a60f433cb0c17eaf6ea47bdd5dfd19dd
Varun Sridharan <varunsridharan23@gmail.com>
2018-06-28 12:46:08 +0530

    Created Few Common Field Array.

--- sample/field_arr.php
+++ sample/field_arr.php
@@ -12,6 +12,14 @@
  * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
+global $wpof;
+
+$wpof = array();
+
+function wponion_card_img() {
+	return 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1644519df77%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1644519df77%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.18333435058594%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E';
+}
+
 function wponion_gif_image( $size = null ) {
 	$size = ( null === $size ) ? wponion_s3_size() : $size;
 	return wponion_s3( $size . '/' . rand( 1, 3 ) . '.gif' );
@@ -63,8 +71,23 @@ function wponion__options( $prefix = '', $max_list = 5, $extra = false ) {
 	return $return;
 }
 
+function wponion__image_select_options( $prefix = '', $image_size = '75', $type = 'jpg' ) {
+	$callback = ( 'jpg' === $type ) ? 'wponion_jpg_image' : 'wponion_gif_image';
+	return array(
+		$prefix . '_image1' => $callback( $image_size ),
+		$prefix . '_image2' => array(
+			'label'   => $callback( $image_size ),
+			'tooltip' => array( 'image' => $callback( $image_size ) ),
+		),
+		$prefix . '_image3' => $callback( $image_size ),
+		$prefix . '_image4' => array(
+			'label'   => $callback( $image_size ),
+			'tooltip' => array( 'image' => $callback( $image_size ) ),
+		),
+	);
+}
 
-$wponion_text_options = wponion_builder()
+$wpof['text'] = wponion_builder()
 	->text( 'textfield1', 'Text Field', array(
 		'transport' => 'postMessage',
 		'output'    => array(
@@ -95,7 +118,7 @@ $wponion_text_options = wponion_builder()
 		),
 	) );
 
-$wponion_textarea = wponion_builder()
+$wpof['textarea'] = wponion_builder()
 	->textarea( 'textarea', 'Textarea' )
 	->textarea( 'textarea_1', 'Textarea With Desc' )
 	->desc( 'Description In Title' )
@@ -103,7 +126,7 @@ $wponion_textarea = wponion_builder()
 	->textarea( 'textarea_2', 'Textarea With Style' )
 	->style( 'border:1px solid red;' );
 
-$wponion_checkbox = wponion_builder()
+$wpof['checkbox'] = wponion_builder()
 	->checkbox( 'checkbox_simple', 'Checkbox Simple', 'Are you sure ?' )
 	->checkbox( 'checkbox_list', 'Checkbox list', wponion__options( false, 3 ) )
 	->desc( 'Simple Description in Title' )
@@ -116,7 +139,7 @@ $wponion_checkbox = wponion_builder()
 		'Group 3' => wponion__options( 'group3', 3 ),
 	) );
 
-$wponion_radio = wponion_builder()
+$wpof['radio'] = wponion_builder()
 	->radio( 'radio_list', 'Checkbox list', wponion__options( false, 3 ) )
 	->desc( 'Simple Description in Title' )
 	->radio( 'radio_long_list', 'Checkbox Long list', wponion__options( false, 2 ) )
@@ -128,29 +151,170 @@ $wponion_radio = wponion_builder()
 		'Group 3' => wponion__options( 'group3', 3 ),
 	) );
 
-$wponion_color_palette = wponion_builder()
+$wpof['select'] = wponion_builder()
+	->select( 'select_list', 'Select List', wponion__options( false, 5 ) )
+	->select( 'select_list_multiple', 'Select List Multiple', wponion__options( false, 5 ) )
+	->multipe()
+	->select( 'select_list_group', 'Select List Group', array(
+		'Group 1' => wponion__options( 'group1', 5 ),
+		'Group 2' => wponion__options( 'group2', 5 ),
+		'Group 4' => wponion__options( 'group3', 5 ),
+	) )
+	->select( 'select_list_multiple_group', 'Select List Multi Group', array(
+		'Group 1' => wponion__options( 'group1', 5 ),
+		'Group 2' => wponion__options( 'group2', 5 ),
+		'Group 4' => wponion__options( 'group3', 5 ),
+	) )
+	->multipe();
+
+$wpof['select_advanced'] = wponion_builder()
+	->subheading( 'Select2 Framework' )
+	->select( 'select_select2', 'Select List (Select2)', wponion__options( false, 5 ), array(
+		'select2' => true,
+		'style'   => 'width:400px',
+	) )
+	->select( 'select_select2_multiple', 'Select List Multiple (Select2)', wponion__options( false, 5 ), array(
+		'select2'  => true,
+		'multiple' => true,
+		'style'    => 'width:400px',
+	) )
+	->select( 'select_select2_theme', 'Select List (Select2) Theme', wponion__options( false, 5 ), array(
+		'select2' => array( 'theme' => 'classic' ),
+		'style'   => 'width:400px',
+	) )
+	->subheading( 'Chosen Framework' )
+	->select( 'select_chosen', 'Select List (Chosen)', wponion__options( false, 5 ), array(
+		'chosen' => true,
+		'style'  => 'width:400px',
+	) )
+	->select( 'select_chosen_multiple', 'Select List Multiple (Chosen)', wponion__options( false, 5 ), array(
+		'chosen'   => true,
+		'multiple' => true,
+		'style'    => 'width:400px',
+	) )
+	->subheading( 'Selectize' )
+	->select( 'select_selectize', 'Select List (Selectize)', wponion__options( false, 5 ), array(
+		'selectize' => true,
+		'style'     => 'width:400px',
+	) )
+	->select( 'select_selectize_multiple', 'Select List Multiple (Selectize)', wponion__options( false, 5 ), array(
+		'selectize' => true,
+		'multiple'  => true,
+		'style'     => 'width:400px',
+	) );
+
+$wpof['color_palette'] = wponion_builder()
 	->color_palette( 'checkbox_palette_1', 'Checkbox Palette', \WPOnion\Helper::get_material_design_colors() )
 	->color_palette( 'radio_palette_1', 'Radio Palette', \WPOnion\Helper::get_material_design_colors(), '30', 'radio' )
 	->color_palette( 'checkbox_palette_2', 'Checkbox Palette Square', \WPOnion\Helper::get_material_design_colors( 'red' ), 25, 'checkbox', 'sqare' )
 	->color_palette( 'radio_palette_2', 'Radio Palette Square', \WPOnion\Helper::get_material_design_colors( 'pink' ), 25, 'radio', 'sqare' );
 
-$wponion_color_picker = wponion_builder()
+$wpof['color_picker'] = wponion_builder()
 	->color_picker( 'color_picker', 'Color Picker', false )
 	->color_picker( 'color_picker2', 'Color Picker 2', true );
 
-$wponion_font_picker = wponion_builder()
+$wpof['font_picker'] = wponion_builder()
 	->font_picker( 'font_picker', 'Font Picker' )
 	->font_picker( 'font_picker_select2', 'Font Picker Select2', array(
-		'select2'    => true,
-		'attributes' => array( 'style' => 'width:200px;' ),
+		'select2' => true,
+		'style'   => 'width:200px;',
 	) )
 	->font_picker( 'font_picker_chosen', 'Font Picker Chosen', array(
-		'chosen'     => true,
-		'attributes' => array( 'style' => 'width:200px;' ),
+		'chosen' => true,
+		'style'  => 'width:200px;',
 	) );
 
-$wponion_images = wponion_builder()
+$wpof['images'] = wponion_builder()
 	->image( 'image_picker', 'Image Picker' )
 	->gallery( 'image_gallery', 'Gallery' );
 
-$wponion_keyval = wponion_builder()->key_value( 'key_value', 'Key Value' );
+$wpof['image_select'] = wponion_builder()
+	->image_select( 'image_select_checkbox', 'Image Select (Checkbox)', wponion__image_select_options() )
+	->image_select( 'image_select_radio', 'Image Select (Radio)', wponion__image_select_options(), 'radio' );
+
+$wpof['key_value'] = wponion_builder()
+	->key_value( 'key_value', 'Key Value' )
+	->key_value( 'key_value_custom_config', 'Key Value Customized', array(
+		'key_input'     => 'USER ID',
+		'value_input'   => 'PASSWORD',
+		'add_button'    => '+',
+		'remove_button' => '(-)',
+	) );
+
+$wpof['icon_picker'] = wponion_builder()
+	->icon_picker( 'icon_picker', 'Icon Picker', true )
+	->icon_picker( 'icon_picker_2', 'Icon Picker2', false );
+
+$wpof['userinterface'] = wponion_builder()
+	->heading( 'This is a simple Big Heading' )
+	->notice_success( 'Here is a success notice' )
+	->subheading( 'This is a sub heading' )
+	->notice_danger( 'Danger Notice' )
+	->notice_dark( 'Dark Notice' )
+	->notice_info( 'Info Notice' )
+	->notice_light( 'Light Notice' )
+	->notice_secondary( 'Secondary Notice' )
+	->notice_warning( 'Warning Notice' )
+	->notice_primary( 'Primary Notice' )
+	->jambo_content( ' <h1 class="display-4">Boostrap Jumbotron</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>' )
+	->card( 'Cards', array(
+		array(
+			'image' => wponion_card_img(),
+			'body'  => '<h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>',
+		),
+		array(
+			'image' => wponion_card_img(),
+			'body'  => '<h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>',
+		),
+		array(
+			'image' => wponion_card_img(),
+			'body'  => '<h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>',
+		),
+		array(
+			'image' => wponion_card_img(),
+			'body'  => '<h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>',
+		),
+	) )
+	->card( false, array(
+		array(
+			'image' => wponion_card_img(),
+			'body'  => '<h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>',
+		),
+		array(
+			'image' => wponion_card_img(),
+			'body'  => '<h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>',
+		),
+		array(
+			'image' => wponion_card_img(),
+			'body'  => '<h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>',
+		),
+		array(
+			'image' => wponion_card_img(),
+			'body'  => '<h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>',
+		),
+		array(
+			'image' => wponion_card_img(),
+			'body'  => '<h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>',
+		),
+	) );
+
+$wpof['wp_links'] = wponion_builder()->wp_link( 'wp_link', 'WP Links' );
+
+$wpof['accordion'] = wponion_builder()
+	->accordion( 'simple_accordion', 'Simple Accordion', wponion_builder()
+		->merge_fields( $wpof['textarea'] )
+		->merge_fields( $wpof['text'] ) )
+	->accordion_title( 'Simple Accordion' );
