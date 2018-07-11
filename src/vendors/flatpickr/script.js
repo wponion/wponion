2816968794a6f6d65e746d27e9d7962173cceb01
Varun Sridharan <varunsridharan23@gmail.com>
2018-07-11 10:42:18 +0530

    added flatpickr styles & scripts

---
+++ src/vendors/flatpickr/script.js
@@ -0,0 +1,4 @@
+//@codekit-append ../../../node_modules/flatpickr/dist/flatpickr.js
+//@codekit-append ../../../node_modules/flatpickr/dist/l10n/*.js
+//@codekit-append ../../../node_modules/flatpickr/dist/plugins/*.js
+//@codekit-append ../../../node_modules/flatpickr/dist/plugins/*/*.js
