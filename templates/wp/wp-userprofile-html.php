81411a8d82f8eb529be1cc5cc97754391746d0b4
Varun Sridharan <varunsridharan23@gmail.com>
2018-07-16 11:41:26 +0530

    Added Userprofile HTML

---
+++ templates/wp/wp-userprofile-html.php
@@ -0,0 +1,13 @@
+<?php
+$settings = $this->user_profile();
+?>
+
+<div class="<?php echo $settings->wrap_class(); ?>">
+
+	<?php
+	foreach ( $settings->fields() as $field ) {
+		echo $settings->render_field( $field );
+	}
+
+	?>
+</div>
