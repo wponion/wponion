3074608aeb91edd46de247ef04cc4c745cf88cd0
Varun Sridharan <varunsridharan23@gmail.com>
2018-07-17 17:23:00 +0530

    Added Flatpickr

--- src/vendors/flatpickr/script.js
+++ src/vendors/flatpickr/script.js
@@ -1,4 +1,4 @@
 //@codekit-append ../../../node_modules/flatpickr/dist/flatpickr.js
-//@codekit-append ../../../node_modules/flatpickr/dist/l10n/*.js
+// @ codekit-append ../../../node_modules/flatpickr/dist/l10n/*.js
 //@codekit-append ../../../node_modules/flatpickr/dist/plugins/*.js
 //@codekit-append ../../../node_modules/flatpickr/dist/plugins/*/*.js
