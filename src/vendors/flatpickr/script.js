7a53d042a76c9e06dc48dc241bfc8c55477f6999
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-29 19:42:52 +0530

    Updated ColorPicker Updated Flatpickr

--- src/vendors/flatpickr/script.js
+++ src/vendors/flatpickr/script.js
@@ -1,4 +1,18 @@
-//@codekit-append ../../../node_modules/flatpickr/dist/flatpickr.js
-// @ codekit-append ../../../node_modules/flatpickr/dist/l10n/*.js
-//@codekit-append ../../../node_modules/flatpickr/dist/plugins/*.js
-//@codekit-append ../../../node_modules/flatpickr/dist/plugins/*/*.js
+//@wponion-append ../../../node_modules/flatpickr/dist/flatpickr.js
+// @ wponion-append ../../../node_modules/flatpickr/dist/l10n/*.js
+// @ wponion-append ../../../node_modules/flatpickr/dist/plugins/*.js
+
+//@wponion-append ../../../node_modules/flatpickr/dist/plugins/minMaxTimePlugin.js
+//@wponion-append ../../../node_modules/flatpickr/dist/plugins/rangePlugin.js
+//@wponion-append ../../../node_modules/flatpickr/dist/plugins/scrollPlugin.js
+//@wponion-append ../../../node_modules/flatpickr/dist/plugins/confirmDate/confirmDate.js
+//@wponion-append ../../../node_modules/flatpickr/dist/plugins/monthSelect/monthSelect.js
+//@wponion-append ../../../node_modules/flatpickr/dist/plugins/labelPlugin/labelPlugin.js
+//@wponion-append ../../../node_modules/flatpickr/dist/plugins/weekSelect/weekSelect.js
+
+
+
+
+
+
+
