ff900120ef83952807e2060e8eee8289190ddc26
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-29 19:44:57 +0530

    Updated Version Number

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPOnion
  * Plugin URI: https://github.com/wponion/
  * Description: Powerfull,Reliable & Lightweight Options Framework.
- * Version: 0.0.2
+ * Version: 0.0.3
  * Author: varunsridharan
  * Author URI: https://varunsridharan.in
  * Text Domain: wponion
