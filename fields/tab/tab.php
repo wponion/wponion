6419f84e8c858dfe40fbcbb3f04dce65cd7a3da8
Varun Sridharan <varunsridharan23@gmail.com>
2018-10-29 19:38:39 +0530

    Issue With Tab First field render fixed

--- fields/tab/tab.php
+++ fields/tab/tab.php
@@ -52,7 +52,9 @@ if ( ! class_exists( '\WPOnion\Field\tab' ) ) {
 				$content .= '<div id="wponion-tab-' . $slug . '" class="wponion-tab-page">';
 
 				foreach ( $section['fields'] as $field ) {
-					$content .= $this->sub_field( $field, wponion_get_field_value( $field, $this->value() ), $unique );
+					$this->catch_output( 'start' );
+					echo $this->sub_field( $field, wponion_get_field_value( $field, $this->value() ), $unique );
+					$content .= $this->catch_output( 'stop' );
 				}
 
 				$content .= '</div>';
