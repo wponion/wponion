2997f32cf3a750058aabe7b79ffd93f8afc5f178
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-08 11:30:43 +0530

    Deleted Old Sourcecode.

--- templates/wp/wp-dashboard-html.php
+++
@@ -1,8 +0,0 @@
-<?php
-global $wponion_wp_theme;
-$settings = $wponion_wp_theme->dashboard_widgets();
-?>
-
-<div class="<?php echo $settings->wrap_class( '', true ); ?>">
-	<?php echo $settings->render(); ?>
-</div>
