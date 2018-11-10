d2bab8e2efe194c6120532a2f3b337fc43c9eca7
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-10 17:32:03 +0530

    added limits and errors

--- src/js/fields/keyvalue_pair.js
+++ src/js/fields/keyvalue_pair.js
@@ -4,7 +4,7 @@ export default class extends WPOnion_Field {
 	init() {
 		this.element.find( '.wponion-keyvalue_wrap' ).WPOnionCloner( {
 			add_btn: this.element.find( '.wponion-fieldset > .wponion-keyvalue-action-container  > button[data-wponion-keyvalue-add]' ),
-			limit: false,
+			limit: ( -1 === this.option( 'limit' ) ) ? null : this.option( 'limit' ),
 			clone_elem: '> .wponion-fieldset > .wponion-keyvalue-field',
 			remove_btn: '.wponion-keyvalue-field > button[data-wponion-keyvalue-remove]',
 			template: this.option( 'html_template' ),
@@ -13,7 +13,21 @@ export default class extends WPOnion_Field {
 				$elem.parent().remove();
 				this.hook.doAction( 'wponion_key_value_updated', $elem );
 			},
+			onLimitReached: () => {
+				let $html = jQuery( '<div class="alert alert-warning" role="alert">' + this.option( 'error_msg' ) + '</div>' )
+					.hide();
+				this.element.find( '.wponion-keyvalue_wrap' ).after( $html );
+				this.element.find( 'div.alert' ).fadeIn( function() {
+					let $__E = jQuery( this );
+					setTimeout( function() {
+						$__E.fadeOut( 'slow', function() {
+							$__E.remove();
+						} );
+					}, 1000 );
+				} );
+			}
 		} );
 	}
 }
+
 
