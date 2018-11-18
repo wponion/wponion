f2469177ab085da2f5536050a6c9c1d8e94a2277
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-18 17:46:21 +0530

    0.0.6 Beta Released

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPOnion
  * Plugin URI: https://github.com/wponion/
  * Description: Powerfull,Reliable & Lightweight Options Framework.
- * Version: 0.0.4
+ * Version: 0.0.6
  * Author: varunsridharan
  * Author URI: https://varunsridharan.in
  * Text Domain: wponion
