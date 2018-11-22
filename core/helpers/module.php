6d458a32cb5e651f6a593f66f7e9f3028683283d
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-22 06:38:13 +0530

    added admin bar module function

--- core/helpers/module.php
+++ core/helpers/module.php
@@ -190,3 +190,14 @@ if ( ! function_exists( 'wponion_bulk_edit' ) ) {
 		return new \WPOnion\Modules\Bulk_Edit( $settings, $fields );
 	}
 }
+
+if ( ! function_exists( 'wponion_admin_bar' ) ) {
+	/**
+	 * @param $admin_bar_menus
+	 *
+	 * @return \WPOnion\Modules\Admin_Bar
+	 */
+	function wponion_admin_bar( $admin_bar_menus ) {
+		return new \WPOnion\Modules\Admin_Bar( $admin_bar_menus );
+	}
+}
