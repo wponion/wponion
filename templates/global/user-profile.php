a90b36a1ce03defd132eac9fda206d2d675d0229
Varun Sridharan <varunsridharan23@gmail.com>
2018-11-24 10:19:55 +0530

    added option to set a custom heading

--- templates/global/user-profile.php
+++ templates/global/user-profile.php
@@ -1,5 +1,8 @@
 <?php $settings = $this->user_profile(); ?>
 
+<?php if ( ! empty( $settings->option( 'heading' ) ) ) { ?>
+	<h2><?php echo $settings->option( 'heading' ); ?></h2>
+<?php } ?>
 <div class="<?php echo $settings->wrap_class(); ?>">
 	<?php
 	foreach ( $settings->fields() as $field ) {
