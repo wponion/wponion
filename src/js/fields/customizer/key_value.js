6ae855c27afc80f4ccf381eda6c892847ac0b892
Varun <varunsridharan23@gmail.com>
2019-01-09 10:43:13 +0530

    Updated Hook Variable

--- src/js/fields/customizer/key_value.js
+++ src/js/fields/customizer/key_value.js
@@ -4,7 +4,7 @@ export default ( ( wp ) => {
 	wp.customize.control.extend( {
 		ready: function() {
 			let $control = this;
-			wp.hooks.addAction( 'wponion_key_value_updated', 'wponion_core', ( ( $elem ) => {
+			window.wponion.hooks.addAction( 'wponion_key_value_updated', 'wponion_core', ( ( $elem ) => {
 				$control.setting.set( WPOnion_Customizer_Module.get_keyval_data( $control ) );
 			} ), 11 );
 		}
