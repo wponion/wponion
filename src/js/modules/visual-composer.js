e6cdf31e6bce2a9fdc6f40f4cba777cc610ac5e8
Varun <varunsridharan23@gmail.com>
2019-01-12 07:45:26 +0530

    * Updated WPOnion_Module_VC Class * renamed wponion_visual_composer_init to wponion_vc_init * Added wponion_vc_field function * Created Gloabl Variable window.wponion.vc

--- src/js/modules/visual-composer.js
+++ src/js/modules/visual-composer.js
@@ -1,18 +1,39 @@
-
-class WPOnion_Visual_Composer {
+class WPOnion_Module_VC {
 	constructor( $element ) {
 		this.element = $element;
+		window.wponion_field( this.element ).reload();
+		window.wponion_vc_field( this.element ).reload();
 	}
 }
 
 export default ( ( window ) => {
-	window.wponion_visual_composer_init = () => {
-		let $element = jQuery( '.wponion-framework.wponion-module-visual_composer' );
+	jQuery( window ).on( 'load', () => {
+		window.wponion.vc = {
+			fields: {
+				abstract: require( './visual-composer/field' ).default,
+				checkbox: require( './visual-composer/checkbox' ).default,
+			},
+		};
 
-		if( $element.length > 0 ) {
-			$element.each( function() {
-				new WPOnion_Visual_Composer( jQuery( this ) );
-			} );
-		}
-	};
+		/**
+		 * Simple Function To Init WPonion VC Module.
+		 */
+		window.wponion_vc_init = () => {
+			let $element = jQuery( '.wponion-framework.wponion-module-vc' );
+
+			if( $element.length > 0 ) {
+				$element.each( function() {
+					new WPOnion_Module_VC( jQuery( this ) );
+				} );
+			}
+		};
+
+		/**
+		 * WPonion VC Field Class Insstance Creator.
+		 * @param $elem
+		 * @param $contxt
+		 * @returns {window.wponion.vc.fields.abstract}
+		 */
+		window.wponion_vc_field = ( $elem, $contxt = {} ) => new window.wponion.vc.fields.abstract( $elem, $contxt );
+	} );
 } )( window );
