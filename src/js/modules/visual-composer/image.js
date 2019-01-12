89ecabac4c97dc1af1081657b2ed51c2bc87a4c9
Varun <varunsridharan23@gmail.com>
2019-01-12 13:21:37 +0530

    added support for image_upload & upload field

---
+++ src/js/modules/visual-composer/image.js
@@ -0,0 +1,17 @@
+import WPOnion_VC_Field from './field';
+
+class field extends WPOnion_VC_Field {
+	/**
+	 * Inits Field.
+	 */
+	init() {
+		if( this.is_vc_param_elem() ) {
+			this.element.find( 'input' ).addClass( 'wpb_vc_param_value' );
+		}
+	}
+}
+
+export default ( ( w ) => {
+	w.wponion_register_field( 'image_upload', ( $elem ) => new field( $elem ), 'vc' );
+	w.wponion_register_field( 'upload', ( $elem ) => new field( $elem ), 'vc' );
+} )( window );
