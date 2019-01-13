ecab4ac71f3f3eb7a384ea849506abe8ab4207e0
Varun <varunsridharan23@gmail.com>
2019-01-13 07:31:06 +0530

    Updated Class to handle custom methods. useful if for integrations with other plugins such as Visual Composer / Brave Builder and so.

--- src/js/core/dependency.js
+++ src/js/core/dependency.js
@@ -8,16 +8,17 @@ export default class {
 	 *
 	 * @param $element
 	 * @param param
+	 * @param $config
 	 */
-	constructor( $element = undefined, param = {} ) {
-		this.param = window.window.wponion._.merge( { nestable: false, parent: false }, param );
+	constructor( $element = undefined, param = {}, $config = {} ) {
+		this.param         = window.wponion._.merge( { nestable: false, parent: false }, param );
 		let $this          = this;
 		this.base          = {};
 		this.base.$el      = $element;
 		this.base.init     = () => {
 			this.base.ruleset = jQuery.deps.createRuleset();
 			this.base.depRoot();
-			jQuery.deps.enable( this.base.$el, this.base.ruleset, {
+			let $_deps_functions = window.wponion._.merge( {
 				show: ( el ) => {
 					el.slideDown();
 					el.find( ':input' ).removeClass( 'wponion-dependent' );
@@ -28,7 +29,9 @@ export default class {
 				},
 				log: false,
 				checkTargets: false,
-			} );
+			}, $config );
+
+			jQuery.deps.enable( this.base.$el, this.base.ruleset, $_deps_functions );
 		};
 		this.base.instance = [];
 		this.base.depRoot  = () => {
