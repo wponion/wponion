35da81e7cad5d879f6aa62a5e1ded76ae4eb0d04
Varun <varunsridharan23@gmail.com>
2019-01-15 08:19:48 +0530

    Updated Style Code.

--- fields/change-log/style1.php
+++ fields/change-log/style1.php
@@ -1,4 +1,28 @@
-<?php /* @var $this \WPOnion\Field\Change_Log */ ?>
+<?php
+/* @var $this \WPOnion\Field\Change_Log */
+
+if ( ! function_exists( 'wponion_change_log_single_log' ) ) {
+	/**
+	 * @param $group
+	 * @param $html
+	 * @param $ins \WPOnion\Field\Change_Log
+	 *
+	 * @return string
+	 */
+	function wponion_change_log_single_log( $group, $html, $ins ) {
+		$label_style = 'background-color:' . $ins->get_group( $group, 'bg' ) . ';';
+		$label_style = $label_style . 'color:' . $ins->get_group( $group, 'color' ) . ';';
+		$label_style = 'style="' . $label_style . '"';
+		$title       = $ins->get_group( $group, 'title' );
+		return '<div class="changelog-log">
+			<div class="changelog-label-container">
+				<div ' . $label_style . ' class="changelog-label changelog-label-' . $group . '">' . $title . '</div>
+			</div>
+			<div class="change-data">' . $html . '</div>
+		</div>';
+	}
+}
+?>
 <h1><?php echo $this->data( 'heading' ); ?></h1>
 <div class="changelog-style1">
 	<div class="changelog-releases">
@@ -10,73 +34,38 @@
 				$date = $data['date'];
 				unset( $data['date'] );
 			}
-			?>
-			<div class="changelog-release">
-				<div class="release-info">
-					<?php if ( ! empty( $version ) ) : ?>
-						<div class="release-version"><?php echo $version; ?></div>
-					<?php endif; ?>
+			echo '<div class="changelog-release"> <div class="release-info">';
+			if ( ! empty( $version ) ) {
+				echo '<div class="release-version">' . $version . '</div>';
+			}
+			if ( ! empty( $date ) ) {
+				echo '<div class="release-date">' . $this->get_formatted_date( $date ) . '</div>';
+			}
 
-					<?php if ( ! empty( $date ) ) : ?>
-						<div class="release-date"><?php echo $this->get_formatted_date( $date ); ?></div>
-					<?php endif; ?>
-				</div>
+			echo '</div> <div class="changelog-changes"> <div class="change-data">';
+			$before = isset( $data['before'] ) ? $data['before'] : false;
+			$after  = isset( $data['after'] ) ? $data['after'] : false;
+			unset( $data['before'] );
+			unset( $data['after'] );
 
-				<div class="changelog-changes">
-					<div class="change-data">
-						<?php
-						$before = isset( $data['before'] ) ? $data['before'] : false;
-						$after  = isset( $data['after'] ) ? $data['after'] : false;
-						unset( $data['before'] );
-						unset( $data['after'] );
+			if ( $before ) {
+				echo '<div class="changelog-before-content"><p>' . $before . '</p></div>';
+			}
 
-						if ( $before ) {
-							echo '<div class="changelog-before-content"><p>' . $before . '</p></div>';
-						}
-						foreach ( $data as $group => $code ) {
-							$label_style = 'background-color:' . $this->get_group( $group, 'bg' ) . ';';
-							$label_style = $label_style . 'color:' . $this->get_group( $group, 'color' ) . ';';
-							$label_style = 'style="' . $label_style . '"';
-							if ( is_array( $code ) ) :
-								foreach ( $code as $html ) {
-									?>
-									<div class="changelog-log">
-										<div class="changelog-label-container">
-											<div <?php echo $label_style; ?>
-												class="changelog-label changelog-label-<?php echo $group; ?>">
-												<?php echo $this->get_group( $group, 'title' ); ?>
-											</div>
-										</div>
-										<div class="change-data"> <?php echo $html; ?></div>
-									</div>
+			foreach ( $data as $group => $code ) {
+				if ( is_array( $code ) ) {
+					foreach ( $code as $html ) {
+						echo wponion_change_log_single_log( $group, $html, $this );
+					}
+				} else {
+					echo wponion_change_log_single_log( $group, $code, $this );
+				}
+			}
 
-
-									<?php
-								}
-							else :
-								?>
-								<div class="changelog-log">
-									<div class="changelog-label-container">
-										<div <?php echo $label_style; ?>
-											class="changelog-label changelog-label-<?php echo $group; ?>">
-											<?php echo $this->get_group( $group, 'title' ); ?>
-										</div>
-									</div>
-									<div class="change-data"> <?php echo $code; ?></div>
-								</div>
-							<?php endif; ?>
-
-							<?php
-						}
-
-						if ( $after ) {
-							echo '<div class="changelog-after-content"><p>' . $after . '</p></div>';
-						}
-						?>
-					</div>
-				</div>
-			</div>
-			<?php
+			if ( $after ) {
+				echo '<div class="changelog-after-content"><p>' . $after . '</p></div>';
+			}
+			echo '</div></div></div>';
 		}
 
 		?>
