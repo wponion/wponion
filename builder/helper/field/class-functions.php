54ceb7e3320a45a2b2065bfa605039a5e293433a
Varun <varunsridharan23@gmail.com>
2019-01-21 10:51:20 +0530

    Updated.

--- builder/helper/field/class-functions.php
+++ builder/helper/field/class-functions.php
@@ -59,12 +59,12 @@ if ( ! trait_exists( '\WPO\Helper\Field\Functions' ) ) {
 		}
 
 		/**
-		 * @param string|\WPO\Field $field_type_or_instance
-		 * @param string            $field_id
-		 * @param bool              $title
-		 * @param array             $args
+		 * @param        $field_type_or_instance
+		 * @param string $field_id
+		 * @param bool   $title
+		 * @param array  $args
 		 *
-		 * @return $this|bool|mixed|\WPO\Field
+		 * @return $this|bool|false|mixed|\WPO\Accordion|\WPO\Background|\WPO\Button|\WPO\Button_Set|\WPO\Change_Log|\WPO\Checkbox|\WPO\Color_Group|\WPO\Color_Palette|\WPO\Color_Picker|\WPO\Content|\WPO\Date_Picker|\WPO\Dimensions|\WPO\Field|\WPO\Fieldset|\WPO\Font_Picker|\WPO\Gallery|\WPO\Google_Maps|\WPO\Group|\WPO\Heading|\WPO\Hidden|\WPO\Icon_Picker|\WPO\Iframe|\WPO\Image|\WPO\Image_Select|\WPO\Input_Group|\WPO\Jambo_Content|\WPO\Key_Value|\WPO\Link_Color|\WPO\Notice|\WPO\Notice_Danger|\WPO\Notice_Dark|\WPO\Notice_Info|\WPO\Notice_Light|\WPO\Notice_Primary|\WPO\Notice_Secondary|\WPO\Notice_Success|\WPO\Notice_Warning|\WPO\Oembed|\WPO\Radio|\WPO\Select|\WPO\Sorter|\WPO\Spacing|\WPO\Subheading|\WPO\Switcher|\WPO\Tab|\WPO\Text|\WPO\Textarea|\WPO\Typography|\WPO\Upload|\WPO\WP_Editor|\WPO\WP_Error_Notice|\WPO\WP_Info_Notice|\WPO\WP_Link|\WPO\WP_List_Table|\WPO\WP_Notice|\WPO\WP_Success_Notice|\WPO\WP_Warning_Notice
 		 */
 		public function field( $field_type_or_instance, $field_id = '', $title = false, $args = array() ) {
 			if ( $this->has_fields() && $this->has_containers() ) {
