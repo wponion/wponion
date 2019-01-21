9eeab00fb87b6df93e4f0d966e0093074d31b95b
Varun <varunsridharan23@gmail.com>
2019-01-21 10:59:11 +0530

    Added Checkbox field builder

---
+++ builder/fields/class-checkbox.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * @link
+ * @copyright 2019 Varun Sridharan
+ * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
+namespace WPO;
+
+if ( ! class_exists( 'WPO\Checkbox' ) ) {
+	/**
+	 * Class Checkbox
+	 *
+	 * @package WPO
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class Checkbox extends Checkbox_Radio {
+		/**
+		 * @var string
+		 * @access
+		 */
+		protected $type = 'checkbox';
+	}
+}
