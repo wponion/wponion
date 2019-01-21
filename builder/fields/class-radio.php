88034ba01f41f2e8ff0657ba02e88dbc6564909e
Varun <varunsridharan23@gmail.com>
2019-01-21 10:59:18 +0530

    Added Radio field builder

---
+++ builder/fields/class-radio.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * @link
+ * @copyright 2019 Varun Sridharan
+ * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
+namespace WPO;
+
+if ( ! class_exists( 'WPO\Radio' ) ) {
+	/**
+	 * Class Radio
+	 *
+	 * @package WPO
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class Radio extends Checkbox_Radio {
+		/**
+		 * @var string
+		 * @access
+		 */
+		protected $type = 'radio';
+	}
+}
