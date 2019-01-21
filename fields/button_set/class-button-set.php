d12ebea57670f34838b01fecffa2ad862c5d799d
Varun <varunsridharan23@gmail.com>
2019-01-21 11:12:30 +0530

    Updated Docs.

--- fields/button_set/class-button-set.php
+++ fields/button_set/class-button-set.php
@@ -15,7 +15,17 @@
 namespace WPOnion\Field;
 
 if ( ! class_exists( '\WPOnion\Field\Button_Set' ) ) {
+	/**
+	 * Class Button_Set
+	 *
+	 * @package WPOnion\Field
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
 	class Button_Set extends Checkbox_Radio {
+		/**
+		 * Renders HTML.
+		 */
 		protected function output() {
 			$this->field['type'] = ( true === $this->data( 'multiple' ) ) ? 'checkbox' : 'radio';
 			echo '<div class="wponion-button-group-container">';
@@ -23,13 +33,24 @@ if ( ! class_exists( '\WPOnion\Field\Button_Set' ) ) {
 			echo '</div>';
 		}
 
+		/**
+		 * @param $label_attr
+		 * @param $field_attr
+		 * @param $value
+		 * @param $attr
+		 * @param $options
+		 *
+		 * @return string
+		 */
 		protected function _element_html( $label_attr, $field_attr, $value, $attr, $options ) {
 			return '<div class="wponion-checker wponion-button-group button ' . $this->data( 'inactive' ) . ' ' . $this->data( 'size' ) . '"><label ' . wponion_array_to_html_attributes( $label_attr ) . '>
 				<input ' . $field_attr . ' ' . $this->checked( $value, $attr['value'], 'checked' ) . '  />' . $options['label'] . '
 			</label></div>';
 		}
 
-
+		/**
+		 * @return array
+		 */
 		protected function js_field_args() {
 			return array(
 				'active'   => $this->data( 'active' ),
@@ -37,6 +58,9 @@ if ( ! class_exists( '\WPOnion\Field\Button_Set' ) ) {
 			);
 		}
 
+		/**
+		 * @return array
+		 */
 		protected function field_default() {
 			return $this->parse_args( array(
 				'multiple' => false,
