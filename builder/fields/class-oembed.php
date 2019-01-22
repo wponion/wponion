cab2f9e2ee5ed2407cbd9e734bd91e45b3d2b27d
Varun <varunsridharan23@gmail.com>
2019-01-22 10:36:27 +0530

    Added OEmbed Builder

---
+++ builder/fields/class-oembed.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * @link
+ * @copyright 2019 Varun Sridharan
+ * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
+namespace WPO;
+
+if ( ! class_exists( 'WPO\OEmbed' ) ) {
+	/**
+	 * Class OEmbed
+	 *
+	 * @package WPO
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class OEmbed extends Text {
+		/**
+		 * OEmbed constructor.
+		 *
+		 * @param bool  $id
+		 * @param bool  $title
+		 * @param array $args
+		 */
+		public function __construct( $id = false, $title = false, $args = array() ) {
+			parent::__construct( $id, $title, $args );
+			$this['type'] = 'oembed';
+		}
+	}
+}
