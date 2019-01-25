bed4ea32a2f5ebfacedc9b20dbaff5c027370101
Varun <varunsridharan23@gmail.com>
2019-01-25 13:59:17 +0530

    updated to work with flexboxgrid

--- src/js/fields/accordion.js
+++ src/js/fields/accordion.js
@@ -31,7 +31,7 @@ class field extends WPOnion_Field {
 	js_error( err ) {
 		let $elem = $wponion.IDtoElement( err.element, this.element );
 		if( $elem ) {
-			err.error.appendTo( $elem.find( '> .wponion-fieldset' ) );
+			err.error.appendTo( $elem.find( '> .row > .wponion-fieldset' ) );
 		}
 	}
 }
