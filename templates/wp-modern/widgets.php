b392c15b29f0aed3b2b9261359f5ce464219e517
Varun <varunsridharan23@gmail.com>
2019-01-26 13:35:22 +0530

    added widget render file.

---
+++ templates/wp-modern/widgets.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var \WPOnion\Theme\WP_Modern $this
+ */
+$widget = $this->widgets();
+$fields = $widget->fields();
+?>
+
+<div class="<?php echo $widget->wrap_class( '' ); ?>">
+	<?php
+	if ( ! empty( $fields->get() ) ) {
+		foreach ( $fields->get() as $field ) {
+			echo $widget->render_field( $field );
+		}
+	}
+	?>
+</div>
