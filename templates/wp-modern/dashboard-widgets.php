e4d607fa37e91b303c2ba0c44d201609a43c11b7
Varun <varunsridharan23@gmail.com>
2019-01-26 10:45:53 +0530

    Created New theme wp_modern

---
+++ templates/wp-modern/dashboard-widgets.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var \WPOnion\Theme\WP_Modern $this
+ */
+$dashboard_widgets = $this->dashboard_widgets();
+$fields            = $dashboard_widgets->fields();
+?>
+
+<div class="<?php echo $dashboard_widgets->wrap_class( '' ); ?>">
+	<?php
+	foreach ( $fields->fields() as $field ) {
+		echo $dashboard_widgets->render_field( $field );
+	}
+	?>
+</div>
