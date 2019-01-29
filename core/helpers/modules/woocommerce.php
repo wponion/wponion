748f9ce7abe48092fe68c304eb4d9d10e8510634
Varun <varunsridharan23@gmail.com>
2019-01-29 13:17:21 +0530

    updated woocommerce module functions.

--- core/helpers/modules/woocommerce.php
+++ core/helpers/modules/woocommerce.php
@@ -1,30 +1,31 @@
 <?php
-if ( ! function_exists( 'wponion_woocommerce_registry' ) ) {
+if ( ! function_exists( 'wponion_woocommerce_product_registry' ) ) {
 	/**
 	 * Creates & Returns an static instance for User Profile. module.
 	 *
 	 * @param $instance
 	 *
-	 * @return bool|\WPOnion\Modules\woocommerce
+	 * @return bool|\WPOnion\Modules\WooCommerce\Product
 	 */
-	function wponion_woocommerce_registry( &$instance ) {
-		return wponion_get_registry_instance( 'woocommerce', $instance, 'module' );
+	function wponion_woocommerce_product_registry( &$instance ) {
+		return wponion_get_registry_instance( 'woocommerce_product', $instance, 'module' );
 	}
 }
 
-if ( ! function_exists( 'wponion_woocommerce' ) ) {
+if ( ! function_exists( 'wponion_wc_product' ) ) {
 	/**
-	 * Returns A New Instance for WooCommerce Module or returns an existing instance.
+	 * Returns A New Instance for WooCommerce Product Module or returns an existing instance.
 	 *
-	 * @param array $instance_id_or_args
-	 * @param array $fields
+	 * @param array             $instance_id_or_args
+	 * @param \WPO\Builder|null $fields
 	 *
-	 * @return bool|\WPOnion\Modules\woocommerce
+	 * @return bool|\WPOnion\Modules\WooCommerce\Product
 	 */
-	function wponion_woocommerce( $instance_id_or_args = array(), $fields = array() ) {
+	function wponion_wc_product( $instance_id_or_args = array(), $fields = null ) {
 		if ( is_string( $instance_id_or_args ) && empty( $fields ) ) {
-			return wponion_woocommerce_registry( $instance_id_or_args );
+			return wponion_woocommerce_product_registry( $instance_id_or_args );
 		}
-		return new \WPOnion\Modules\woocommerce( $instance_id_or_args, $fields );
+		return new \WPOnion\Modules\WooCommerce\Product( $instance_id_or_args, $fields );
 	}
 }
+
