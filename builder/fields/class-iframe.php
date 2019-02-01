16ad1b591f026db1870bf579641899d07983fc98
Varun <varunsridharan23@gmail.com>
2019-02-01 09:14:01 +0530

    added iframe builder.

---
+++ builder/fields/class-iframe.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * @link
+ * @copyright 2019 Varun Sridharan
+ * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
+namespace WPO;
+
+if ( ! class_exists( 'WPO\Iframe' ) ) {
+	/**
+	 * Class Iframe
+	 *
+	 * @package WPO
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class Iframe extends Field {
+		/**
+		 * Iframe constructor.
+		 *
+		 * @param bool  $id
+		 * @param bool  $title
+		 * @param array $args
+		 */
+		public function __construct( $id = false, $title = false, $args = array() ) {
+			parent::__construct( 'iframe', $id, $title, $args );
+		}
+
+		/**
+		 * @param bool $width
+		 *
+		 * @return $this
+		 */
+		public function set_width( $width = true ) {
+			$this['width'] = $width;
+			return $this;
+		}
+
+		/**
+		 * @param bool $height
+		 *
+		 * @return $this
+		 */
+		public function set_height( $height = true ) {
+			$this['height'] = $height;
+			return $this;
+		}
+
+		/**
+		 * @param bool $url
+		 *
+		 * @return $this
+		 */
+		public function set_url( $url = true ) {
+			$this['url'] = $url;
+			return $this;
+		}
+
+		/**
+		 * @param bool $heading
+		 *
+		 * @return $this
+		 */
+		public function set_heading( $heading = true ) {
+			$this['heading'] = $heading;
+			return $this;
+		}
+
+		/**
+		 * @return array
+		 */
+		protected function defaults() {
+			return array(
+				'heading' => false,
+				'url'     => false,
+				'height'  => '100%',
+				'width'   => '100%',
+			);
+		}
+	}
+}
