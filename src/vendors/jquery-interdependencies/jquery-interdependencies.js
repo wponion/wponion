cb7497c6c6557693b7d82fe3be9e11137e4f8706
Varun <varunsridharan23@gmail.com>
2019-02-03 07:00:55 +0530

    Reduced Javascript File Errors.

--- src/vendors/jquery-interdependencies/jquery-interdependencies.js
+++ src/vendors/jquery-interdependencies/jquery-interdependencies.js
@@ -1,3 +1,4 @@
+/* global console:true */
 ( function( $ ) {
 	var $WPONION = [ 'input[name=post_format]', 'select#page_template', 'input#menu_order', 'select#parent_id', 'select#post_status', 'input[name=visibility]' ];
 
@@ -19,21 +20,21 @@
 		},
 
 		evalCondition: function( context, control, condition, val1, val2 ) {
-			if( "==" === condition ||"=" === condition || "OR" === condition ) {
+			if( '==' === condition || '=' === condition || 'OR' === condition ) {
 				return this.checkBoolean( val1 ) === this.checkBoolean( val2 );
-			} else if( "!=" === condition ) {
+			} else if( '!=' === condition ) {
 				return this.checkBoolean( val1 ) !== this.checkBoolean( val2 );
-			} else if( ">=" === condition ) {
+			} else if( '>=' === condition ) {
 				return Number( val2 ) >= Number( val1 );
-			} else if( "<=" === condition ) {
+			} else if( '<=' === condition ) {
 				return Number( val2 ) <= Number( val1 );
-			} else if( ">" === condition ) {
+			} else if( '>' === condition ) {
 				return Number( val2 ) > Number( val1 );
-			} else if( "<" === condition ) {
+			} else if( '<' === condition ) {
 				return Number( val2 ) < Number( val1 );
-			} else if( "()" === condition ) {
+			} else if( '()' === condition ) {
 				return window[ val1 ]( context, control, val2 ); // FIXED: function method
-			} else if( "in" === condition ) {
+			} else if( 'in' === condition ) {
 				if( '' === val2 || null === val2 ) {
 					return false;
 				}
@@ -48,12 +49,12 @@
 
 				}
 				return false;
-			} else if( "any" === condition ) {
+			} else if( 'any' === condition ) {
 				return $.inArray( val2, val1.split( ',' ) ) > -1;
-			} else if( "not-any" === condition ) {
+			} else if( 'not-any' === condition ) {
 				return -1 === $.inArray( val2, val1.split( ',' ) );
 			} else {
-				throw new Error( "Unknown condition:" + condition );
+				throw new Error( 'Unknown condition:' + condition );
 			}
 		},
 
@@ -82,12 +83,12 @@
 
 			var control = context.find( this.controller );
 			if( control.size() === 0 && cfg.log ) {
-				console.log( "Evaling condition: Could not find controller input " + this.controller );
+				console.log( 'Evaling condition: Could not find controller input ' + this.controller );
 			}
 
 			var val = this.getControlValue( context, control );
 			if( cfg.log && val === undefined ) {
-				console.log( "Evaling condition: Could not exctract value from input " + this.controller );
+				console.log( 'Evaling condition: Could not exctract value from input ' + this.controller );
 			}
 			if( val === undefined ) {
 				return false;
@@ -98,19 +99,19 @@
 		},
 
 		normalizeValue: function( control, baseValue, val ) {
-			if( typeof baseValue === "number" ) {
+			if( typeof baseValue === 'number' ) {
 				return parseFloat( val );
 			}
 			return val;
 		},
 
 		getControlValue: function( context, control ) {
-			if( ( "radio" === control.attr( "type" ) || "checkbox" === control.attr( "type" ) ) && control.length > 1 ) {
-				return control.filter( ":checked" ).val();
+			if( ( 'radio' === control.attr( 'type' ) || 'checkbox' === control.attr( 'type' ) ) && control.length > 1 ) {
+				return control.filter( ':checked' ).val();
 			}
 
-			if( "checkbox" === control.attr( "type" ) || "radio" === control.attr( "type" ) ) {
-				return control.is( ":checked" );
+			if( 'checkbox' === control.attr( 'type' ) || 'radio' === control.attr( 'type' ) ) {
+				return control.is( ':checked' );
 			}
 			return control.val();
 		},
@@ -123,7 +124,7 @@
 
 		include: function( input ) {
 			if( !input ) {
-				throw new Error( "Must give an input selector" );
+				throw new Error( 'Must give an input selector' );
 			}
 
 			this.controls.push( input );
@@ -139,11 +140,11 @@
 			}
 
 			if( cfg.log ) {
-				console.log( "Applying rule on " + this.controller + "==" + this.value + " enforced:" + enforced + " result:" + result );
+				console.log( 'Applying rule on ' + this.controller + '==' + this.value + ' enforced: ' + enforced + ' result: ' + result );
 			}
 
 			if( cfg.log && !this.controls.length ) {
-				console.log( "Zero length controls slipped through" );
+				console.log( 'Zero length controls slipped through' );
 			}
 
 			var show = cfg.show || function( control ) {
@@ -155,10 +156,10 @@
 			};
 
 
-			var controls = $.map( this.controls, function( elem, idx ) {
-				var control = ( typeof elem === "object" ) ? elem : context.find( elem );
+			var controls = $.map( this.controls, function( elem ) {
+				var control = ( typeof elem === 'object' ) ? elem : context.find( elem );
 				if( cfg.log && control.size() === 0 ) {
-					console.log( "Could not find element:" + elem );
+					console.log( 'Could not find element:' + elem );
 				}
 				return control;
 			} );
@@ -167,28 +168,28 @@
 				$( controls ).each( function() {
 					// Some friendly debug info
 					if( cfg.log && $( this ).size() === 0 ) {
-						console.log( "Control selection is empty when showing" );
+						console.log( 'Control selection is empty when showing' );
 						console.log( this );
 					}
 					show( this );
 				} );
 
 				$( this.rules ).each( function() {
-					if( this.condition !== "OR" ) {
+					if( this.condition !== 'OR' ) {
 						this.applyRule( context, cfg );
 					}
 				} );
 			} else {
 				$( controls ).each( function() {
 					if( cfg.log && $( this ).size() === 0 ) {
-						console.log( "Control selection is empty when hiding:" );
+						console.log( 'Control selection is empty when hiding:' );
 						console.log( this );
 					}
 					hide( this );
 				} );
 
 				$( this.rules ).each( function() {
-					if( this.condition !== "OR" ) {
+					if( this.condition !== 'OR' ) {
 						this.applyRule( context, cfg, false );
 					} else {
 						this.applyRule( context, cfg );
@@ -210,7 +211,7 @@
 			cfg = cfg || {};
 
 			if( cfg.log ) {
-				console.log( "Starting evaluation ruleset of " + this.rules.length + " rules" );
+				console.log( 'Starting evaluation ruleset of ' + this.rules.length + ' rules' );
 			}
 
 			for( i = 0; i < this.rules.length; i++ ) {
@@ -240,23 +241,23 @@
 
 			$( rules ).each( function() {
 				if( context.find( this.controller ).length === 0 ) {
-					throw new Error( "Rule's controller does not exist:" + this.controller );
+					throw new Error( 'Rule\'s controller does not exist: ' + this.controller );
 				}
 
 				if( this.controls.length === 0 ) {
-					throw new Error( "Rule has no controls:" + this );
+					throw new Error( 'Rule has no controls: ' + this );
 				}
 
 				$( this.controls ).each( function() {
 					if( context.find( this ) === 0 ) {
-						throw new Error( "Rule's target control " + this + " does not exist in context " + context.get( 0 ) );
+						throw new Error( 'Rule\'s target control ' + this + ' does not exist in context ' + context.get( 0 ) );
 					}
 					controls++;
 				} );
 			} );
 
 			if( cfg.log ) {
-				console.log( "Controller check ok, rules count " + rules.length + " controls count " + controls );
+				console.log( 'Controller check ok, rules count ' + rules.length + ' controls count ' + controls );
 			}
 		},
 
@@ -278,13 +279,13 @@
 			}
 
 			if( cfg.log ) {
-				console.log( "Enabling dependency change monitoring on " + selection.get( 0 ) );
+				console.log( 'Enabling dependency change monitoring on ' + selection.get( 0 ) );
 			}
 
 			var handler = function() {
 				ruleset.applyRules( selection, cfg );
 			};
-			var val     = selection.on( "change.deps", null, null, handler );
+			var val     = selection.on( 'change.deps', null, null, handler );
 
 			ruleset.applyRules( selection, cfg );
 			return val;
@@ -299,7 +300,7 @@
 			}
 
 			if( cfg.log ) {
-				log( "Enabling dependency change monitoring on " + selection.get( 0 ) );
+				console.log( 'Enabling dependency change monitoring on ' + selection.get( 0 ) );
 			}
 
 			var handler = function() {
@@ -311,7 +312,7 @@
 					handler();
 				} );
 			} );
-			var val     = selection.on( "change.deps", null, null, handler );
+			var val = selection.on( 'change.deps', null, null, handler );
 			ruleset.applyRules( selection, cfg );
 			return val;
 		}
@@ -332,22 +333,21 @@
 				return true;
 			}
 
+			var control = context.find( this.controller );
 			if( $.inArray( this.controller, $WPONION ) !== -1 ) {
-				var control = $( "body" ).find( this.controller );
+				control = $( 'body' ).find( this.controller );
 
-			} else {
-				var control = context.find( this.controller );
 			}
 
 
 			if( control.length === 0 && cfg.log ) {
-				log( "Evaling condition: Could not find controller input " + this.controller );
+				console.log( 'Evaling condition: Could not find controller input ' + this.controller );
 			}
 
 			var val = this.FieldVal( context, control );
 
 			if( cfg.log && val === undefined ) {
-				log( "Evaling condition: Could not extract value from input " + this.controller );
+				console.log( 'Evaling condition: Could not extract value from input ' + this.controller );
 			}
 
 			if( val === false ) {
@@ -356,8 +356,5 @@
 
 			return this.evalCondition( context, control, this.condition, this.value, val );
 		},
-
 	} );
-
-
 } )( jQuery );
