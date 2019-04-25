025ab231be734f021a086205ae6749bc74efa40c
Varun <varunsridharan23@gmail.com>
2019-04-25 07:09:29 +0530

    Removed * Fresh * modern * wp Themes

--- templates/wp/wp-help-tabs.php
+++
@@ -1,9 +0,0 @@
-<?php $settings = $this->help_tabs(); ?>
-
-<div class="<?php echo $settings->wrap_class(); ?>">
-	<?php
-	foreach ( $settings->current_tabs['fields'] as $field ) {
-		echo $settings->render_field( $field );
-	}
-	?>
-</div>
