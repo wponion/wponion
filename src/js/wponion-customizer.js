fdadc10391029807dd3c67c681bce481bd113f7a
Varun <varunsridharan23@gmail.com>
2019-04-29 17:55:39 +0530

    Customizer JS Fully Reworked.

--- src/js/wponion-customizer.js
+++ src/js/wponion-customizer.js
@@ -29,20 +29,53 @@
 			this.container.on( 'wponion_field_updated', () => $wpoch.save( this ) );
 		}
 	} );
+	$wpcc.wponion_field_fieldset    = $wpcc.wponion_field_clone;
+	$wpcc.wponion_field_accordion   = $wpcc.wponion_field_clone;
+	$wpcc.wponion_field_group       = $wpcc.wponion_field_clone;
+	$wpcc.wponion_field_icon_picker = $wpcc.wponion_field_clone;
 
+	/**
+	 * Field Checkbox.
+	 */
 	$wpcc.wponion_field_checkbox = $wpc.extend( {
 		ready: function() {
 			this.container.on( 'change', () => $wpoch.save( this ) );
 		},
-	});
+	} );
+	$wpcc.wponion_field_radio        = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_button_set   = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_color_picker = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_input_group  = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_font_picker  = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_date_picker  = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_image_select = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_image        = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_gallery      = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_wp_link      = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_background   = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_color_group  = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_link_color   = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_spacing      = $wpcc.wponion_field_checkbox;
+	$wpcc.wponion_field_dimensions   = $wpcc.wponion_field_checkbox;
 
-
-	$wpcc.wponion_field_text = $wpc.extend( {
+	/**
+	 * Field Sorter.
+	 */
+	$wpcc.wponion_field_sorter = $wpc.extend( {
 		ready: function() {
-			/**/
+			this.container.on( 'wponion_field_updated', () => $wpoch.save( this ) );
 		}
 	} );
 
+	/**
+	 * Key Value Pair.
+	 */
+	$wpcc.wponion_field_key_value = $wpc.extend( {
+		ready: function() {
+			this.container.on( 'change', ':input', () => $wpoch.save( this ) );
+			this.container.on( 'wponion_field_updated', () => $wpoch.save( this ) );
+		}
+	} );
 
 	/**
 	 * Inits Customizer Instance.
