0e905761d8549b35287649f6f1e85802a7e20d12
Varun <varunsridharan23@gmail.com>
2019-04-29 11:55:37 +0530

    Removed Unused Files.

--- src/js/wponion-customizer.js
+++ src/js/wponion-customizer.js
@@ -1,128 +1,56 @@
 ( ( window, document, $, wp ) => {
-	let wphooks = window.wponion.hooks;
-
-	/**
-	 * Customizer Functions.
-	 * @type {{link_customize_settings: function(*), cloneable_update: function(*), cloneable: function(*=)}}
-	 */
-	let $wponion_customizer = {
-		/**
-		 * Adds data-customize-settings-link attribute.
-		 */
-		link_customize_settings: ( ( $elem ) => {
-			$elem.find( 'input , textarea' ).each( function() {
-				$( this ).attr( 'data-customize-setting-link', true );
-			} );
-		} ),
-
-		/**
-		 * Links WIth KeyValue to auto get and save data.
-		 */
-		cloneable_update: ( ( $control ) => {
-			let $values = $control.container.find( ':input' )
-								  .inputToArray( { key: 'name', value: true } );
-			let $input  = $control.container.find( 'input.wponion_cloneable_value' );
-
-			$.each( $values, function( $k, $vs ) {
-				$.each( $vs, function( $e, $ep ) {
-					$values = $ep;
+	let wphooks = window.wponion.hooks,
+		$wpcc   = wp.customize.controlConstructor,
+		$wpc    = wp.customize.Control,
+		$wpoch  = {
+			values: ( $values ) => {
+				$.each( $values, function( $k, $vs ) {
+					$.each( $vs, function( $e, $ep ) {
+						$values = $ep;
+					} );
 				} );
-			} );
-
-			$input.val( JSON.stringify( $values ) );
-			$input.trigger( 'change' );
-		} ),
-
-		/**
-		 * Links WIth KeyValue to auto get and save data.
-		 */
-		get_keyval_data: ( ( $control ) => {
-			let $values = $control.container.find( ':input' )
-								  .inputToArray( { key: 'name', value: true } );
-			$.each( $values, function( $k, $vs ) {
-				$.each( $vs, function( $e, $ep ) {
-					$values = $ep;
-				} );
-			} );
-
-			return $values;
-		} ),
-
-		/**
-		 * Enables Cloneable fields.
-		 */
-		cloneable: ( ( $control ) => {
-			$control.container.on( 'change', ':input', function() {
-				if( !$( this ).hasClass( 'wponion_cloneable_value' ) ) {
-					$wponion_customizer.cloneable_update( $control );
+				return $values;
+			},
+			save: ( $instance, $value = undefined ) => {
+				if( window.wponion._.isUndefined( $value ) ) {
+					$value = $wpoch.values( $instance.container.find( ':input' )
+													 .serializeJSON() );
 				}
-			} );
-		} ),
-	};
-
-	let $wpc = wp.customize.controlConstructor;
+				$instance.setting.set( $value );
+			},
+		};
 
 	/**
-	 * Handles Key Value field in customizer.
+	 * Field Clone.
 	 */
-	$wpc.wponion_field_key_value = wp.customize.Control.extend( {
+	$wpcc.wponion_field_clone = $wpc.extend( {
 		ready: function() {
-			let control = this;
-			wphooks.addAction( 'wponion_key_value_updated', 'wponion_core', ( () => {
-				let $val = $wponion_customizer.get_keyval_data( control );
-				control.setting.set( $val );
-			} ), 11 );
-
-			control.container.on( 'change', 'input[type=text]', function() {
-				let $val = $wponion_customizer.get_keyval_data( control );
-				control.setting.set( $val );
-			} );
+			this.container.on( 'change', () => $wpoch.save( this ) );
+			this.container.on( 'wponion_field_updated', () => $wpoch.save( this ) );
 		}
 	} );
 
-	/**
-	 * Handles Checkbox Field in Customizer.
-	 */
-	$wpc.wponion_field_checkbox = wp.customize.Control.extend( {
+	$wpcc.wponion_field_checkbox = $wpc.extend( {
 		ready: function() {
-			let control = this;
-			control.container.on( 'change', ':input', function() {
-				let $val = $wponion_customizer.get_keyval_data( control );
-				control.setting.set( $val );
-			} );
-		}
-	} );
-
-	/**
-	 * Handles Fieldset And Checkbox Field.
-	 */
-	$wpc.wponion_field_fieldset = wp.customize.Control.extend( {
-		ready: function() {
-			$wponion_customizer.cloneable( this );
-		}
-	} );
-
-	/**
-	 * Handles Image Picker.
-	 */
-	$wpc.wponion_field_image = $wpc.wponion_field_gallery = wp.customize.Control.extend( {
-		initialize: function( id, options ) {
-			let $html  = $( '<div>' + options.params.content + '</div>' );
-			let $input = $html.find( 'input#image_id' );
-			$input.attr( 'data-customize-setting-link', $html.find( 'input#image_id' ).attr( 'name' ) );
-			options.params.content = $html.html();
-			wp.customize.Control.prototype.initialize.call( this, id, options );
+			this.container.on( 'change', () => $wpoch.save( this ) );
 		},
+	});
+
+
+	$wpcc.wponion_field_text = $wpc.extend( {
+		ready: function() {
+			/**/
+		}
 	} );
+
 
 	/**
 	 * Inits Customizer Instance.
 	 */
 	wphooks.addAction( 'wponion_init', 'wponion_core', ( () => {
 		$( '.wponion-module-customizer-framework.wponion-framework' ).each( function() {
-			$wponion_customizer.link_customize_settings( $( this ) );
+
 		} );
 	} ) );
 
 } )( window, document, jQuery, wp );
-
