aeeb6fa5dc8ab1c5a8b5781605289440833c1842
Varun <varunsridharan23@gmail.com>
2019-04-29 11:54:49 +0530

    Added Customizer Module Fields.

---
+++ module_fields/customizer/class-cloner.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WPOnion\Module_Fields\Customizer;
+
+use WPOnion\Modules\Customizer\Control as Control;
+
+if ( ! class_exists( '\WPOnion\Module_Fields\Customizer\Cloner' ) ) {
+	/**
+	 * Class Cloner
+	 *
+	 * @package WPOnion\Module_Fields\Customizer
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class Cloner extends Control {
+		/**
+		 * @var bool
+		 * @access
+		 */
+		protected $link_attr = false;
+
+		/**
+		 * Returns Element Value.
+		 *
+		 * @return mixed
+		 */
+		protected function el_value() {
+			return array_filter( $this->value() );
+		}
+	}
+}
