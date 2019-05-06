659440830c21e6bf728c433c6d8ed74ce838946a
Varun <varunsridharan23@gmail.com>
2019-05-06 11:37:39 +0530

    Added Endpoint Feature.

--- core/helpers/modules/simple-module.php
+++ core/helpers/modules/simple-module.php
@@ -10,3 +10,15 @@ if ( ! function_exists( 'wponion_plugin_links' ) ) {
 		return new \WPOnion\Modules\Util\Plugin_Links( $plugin_file );
 	}
 }
+
+
+if ( ! function_exists( 'wponion_endpoint' ) ) {
+	/**
+	 * @param $slug
+	 *
+	 * @return \WPOnion\Modules\Util\Endpoint
+	 */
+	function wponion_endpoint( $slug ) {
+		return new \WPOnion\Modules\Util\Endpoint( $slug );
+	}
+}
