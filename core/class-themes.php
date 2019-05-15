3d7d1073244a2669c0c025a4c1280059f64c4ccd
Varun <varunsridharan23@gmail.com>
2019-05-15 10:04:41 +0530

    Added User Profile Support For 1. WC 2. WP 3. WP_Lite Themes

--- core/class-themes.php
+++ core/class-themes.php
@@ -90,9 +90,20 @@ if ( ! class_exists( '\WPOnion\Themes' ) ) {
 				'widget',
 				'user_profile',
 			), 'wponion_wp_modern_theme_init' );
-			self::register( 'wp_lite', array( 'settings' ), 'wponion_wp_lite_theme_init' );
-			self::register( 'wp', array( 'settings', 'metabox' ), 'wponion_wp_theme_init' );
-			self::register( 'wc', array( 'metabox', 'settings' ), 'wponion_wc_theme_init' );
+			self::register( 'wp_lite', array(
+				'settings',
+				'user_profile',
+			), 'wponion_wp_lite_theme_init' );
+			self::register( 'wp', array(
+				'settings',
+				'metabox',
+				'user_profile',
+			), 'wponion_wp_theme_init' );
+			self::register( 'wc', array(
+				'metabox',
+				'settings',
+				'user_profile',
+			), 'wponion_wc_theme_init' );
 		}
 
 		/**
---
+++ templates/wp/user-profile.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * @link
+ * @copyright 2019 Varun Sridharan
+ * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
+require_once WPONION_PATH . 'templates/global/user-profile.php';
