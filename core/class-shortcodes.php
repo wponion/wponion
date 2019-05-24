31b112c8b75512c6fc51266b4041949041291b65
Varun <varunsridharan23@gmail.com>
2019-05-24 22:29:18 +0530

    Added logic shortcode

--- core/class-shortcodes.php
+++ core/class-shortcodes.php
@@ -32,6 +32,9 @@ if ( ! class_exists( '\WPOnion\Shortcodes' ) ) {
 		 */
 		public function __construct() {
 			add_shortcode( 'wpo_tooltip', array( &$this, 'tooltip' ) );
+			add_shortcode( 'logic', array( &$this, 'wp_if_condition' ) );
+			add_shortcode( 'wp_logic', array( &$this, 'wp_if_condition' ) );
+			add_shortcode( 'wpo_logic', array( &$this, 'wp_if_condition' ) );
 		}
 
 		/**
@@ -50,6 +53,50 @@ if ( ! class_exists( '\WPOnion\Shortcodes' ) ) {
 			}
 			return $content;
 		}
+
+		/**
+		 * @param $args
+		 * @param $content
+		 *
+		 * @return string
+		 */
+		public function wp_if_condition( $args, $content ) {
+			$args = shortcode_atts( array(
+				'rule'      => false,
+				'value'     => null,
+				'condition' => 'and',
+				'compare'   => '=',
+				'extra'     => '',
+				'seperator' => '|',
+			), $args, 'wp-logic' );
+
+			$new_rules = array();
+
+			if ( ! empty( $args['rule'] ) ) {
+				$args['rule']    = explode( $args['seperator'], $args['rule'] );
+				$args['value']   = explode( $args['seperator'], $args['value'] );
+				$args['compare'] = explode( $args['seperator'], $args['compare'] );
+				$args['extra']   = explode( $args['seperator'], $args['extra'] );
+				foreach ( $args['rule'] as $i => $rule ) {
+					$new_rules[] = array(
+						'rule'    => $rule,
+						'compare' => isset( $args['compare'][ $i ] ) ? $args['compare'][ $i ] : '=',
+						'extra'   => isset( $args['extra'][ $i ] ) ? $args['extra'][ $i ] : null,
+						'value'   => isset( $args['value'][ $i ] ) ? wponion_validate_bool_val( $args['value'][ $i ] ) : null,
+					);
+				}
+			}
+			var_dump( $content );
+			if ( ! empty( $new_rules ) ) {
+				$builder = wp_conditional_logic_builder( $args['condition'] );
+				foreach ( $new_rules as $rule ) {
+					$builder->rule( $rule['rule'], $rule['value'], $rule['compare'], $rule['extra'] );
+				}
+				return ( $builder->run() ) ? do_shortcode( $content, true ) : '';
+			}
+			return do_shortcode( $content, true );
+		}
+
 	}
 }
 return new Shortcodes;
