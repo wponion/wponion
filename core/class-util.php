8e679dc5c2be986a22662aac184fe21b973e01ae
Varun <varunsridharan23@gmail.com>
2019-05-30 12:12:01 +0530

    1. Created A Util Class 2. Updated ToolTip Creator 3. Updated Ajax Creator 4. Added Image POPUP

---
+++ core/class-util.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ *
+ * Initial version created 15-05-2018 / 10:30 AM
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * @package
+ * @link
+ * @copyright 2018 Varun Sridharan
+ * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
+namespace WPOnion;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+if ( ! class_exists( '\WPOnion\Util' ) ) {
+	/**
+	 * Class Util
+	 *
+	 * @package WPOnion\JS
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class Util {
+		/**
+		 * Stores Fields JS id.
+		 *
+		 * @var bool
+		 * @access
+		 */
+		protected $js_id = false;
+
+		/**
+		 * Stores Element HTML.
+		 *
+		 * @var null
+		 * @access
+		 */
+		protected $element = null;
+
+		/**
+		 * Util constructor.
+		 *
+		 * @param bool|string $element
+		 * @param bool|string $js_id
+		 */
+		public function __construct( $element = false, $js_id = false ) {
+			$this->element = $element;
+			$this->js_id   = $js_id;
+		}
+
+		/**
+		 * @return bool|string
+		 */
+		public function element() {
+			return $this->element;
+		}
+
+		/**
+		 * Handles Fields JS ID.
+		 *
+		 * @param $element
+		 * @param $js_id
+		 */
+		protected function handle_element( $element, $js_id = false ) {
+			$js_id = ( $js_id === $this->js_id ) ? false : $js_id;
+
+			if ( $element instanceof Util ) {
+				$this->element = $element->element();
+				$this->js_id   = $element->js_id();
+			} elseif ( is_string( $element ) ) {
+				$this->element = $element;
+				$re            = '/(?:data-wponion-jsid)="(.+?)"/m';
+				preg_match( $re, $element, $matches, PREG_OFFSET_CAPTURE, 0 );
+				if ( isset( $matches[1][0] ) && ! empty( $matches[1][0] ) ) {
+					$this->js_id = trim( $matches[1][0] );
+				}
+			}
+
+			if ( ! empty( $js_id ) ) {
+				$this->js_id = $js_id;
+			}
+		}
+
+		/**
+		 * Returns Element String.
+		 *
+		 * @return bool|string|null
+		 */
+		public function __toString() {
+			return ( ! empty( $this->element ) ) ? $this->element : '';
+		}
+
+		/**
+		 * Returns JS Id.
+		 *
+		 * @return bool|string
+		 */
+		public function js_id() {
+			if ( empty( $this->js_id ) ) {
+				$this->js_id = 'wpo' . wponion_hash_string( $this->element . microtime( true ) );
+			}
+			return $this->js_id;
+		}
+
+		/**
+		 * @param      $content
+		 * @param      $args
+		 * @param bool $localize
+		 *
+		 * @return array|mixed
+		 */
+		public function tooltip( $content, $args, $localize = true ) {
+			$args = wp_parse_args( $args, array(
+				'content'     => $content,
+				'image'       => false,
+				'arrow'       => true,
+				'arrowType'   => 'round',
+				'js_field_id' => $this->js_id(),
+				'element'     => $this->element,
+				'placement'   => 'top',
+			) );
+
+			$this->js_id   = $args['js_field_id'];
+			$this->element = $args['element'];
+			unset( $args['js_field_id'] );
+			unset( $args['element'] );
+
+			if ( false === $args['image'] && true === wponion_is_url( $args['content'] ) ) {
+				$args['image']   = $args['content'];
+				$args['content'] = false;
+			}
+
+			$attr = array( 'wponion-help' => 'wponion-help' );
+
+			if ( false !== $localize ) {
+				$localize                        = ( true === $localize ) ? 'wponion-help' : $localize;
+				$localize                        = ( false !== $this->element ) ? md5( $this->element ) : $localize;
+				$attr['data-wponion-tooltip-id'] = $localize;
+				wponion_localize()->add( $this->js_id, array( $localize => $args ) );
+			} else {
+				$attr['data-tippy'] = $args['content'];
+			}
+
+			if ( false === $this->element ) {
+				return array(
+					'attr' => $attr,
+					'data' => $args,
+				);
+			}
+
+			$attr['data-wponion-jsid'] = $this->js_id;
+			preg_match_all( '/^(.?)<[a-z][a-z0-9]*?\b/s', $this->element, $matches, PREG_SET_ORDER, 0 );
+			if ( isset( $matches[0][0] ) ) {
+				$this->element = str_replace( $matches[0][0], $matches[0][0] . ' ' . wponion_array_to_html_attributes( $attr ) . ' ', $this->element );
+			}
+			return $this;
+		}
+
+		/**
+		 * @param $args
+		 *
+		 * @return $this|bool|string
+		 */
+		public function inline_ajax( $args ) {
+			$args = wp_parse_args( $args, array(
+				'method'      => 'post',
+				'url'         => admin_url( 'admin-ajax.php' ),
+				'part_url'    => false,
+				'data'        => array(),
+				'success'     => false,
+				'error'       => false,
+				'always'      => false,
+				'js_field_id' => $this->js_id(),
+				'element'     => $this->element(),
+			) );
+
+			$this->handle_element( $args['element'], $args['js_field_id'] );
+
+			unset( $args['button'] );
+			unset( $args['js_field_id'] );
+			wponion_localize()->add( $this->js_id(), array( 'inline_ajax' => $args ) );
+			if ( ! empty( $this->element ) ) {
+				$this->element = preg_replace( '/(<[a-zA-Z0-9]* )(.*)/', '$1 data-wponion-inline-ajax="' . $this->js_id() . '" $2', $this->element );
+				return $this;
+			}
+			return $this->js_id();
+		}
+
+		/**
+		 * @param      $image_src
+		 * @param      $full_size
+		 * @param bool $element
+		 *
+		 * @return $this
+		 */
+		public function image_popup( $image_src, $full_size, $element = false ) {
+			if ( false === $element && true !== wponion_is_url( $image_src ) ) {
+				$element   = $image_src;
+				$image_src = false;
+			}
+
+			if ( ! empty( $full_size ) ) {
+				$full_size = "data-fullsize='" . $full_size . "' ";
+			}
+
+			$full_size .= '  wponion-img-popup="wponion-img-popup" ';
+
+			$this->handle_element( $element );
+
+			if ( ! empty( $this->element ) ) {
+				$this->element = preg_replace( '/(<[a-zA-Z0-9]* )(.*)/', '$1 ' . $full_size . '  $2', $this->element );
+			} else {
+				$this->element = '<img src="' . $image_src . '" ' . $full_size . ' />';
+			}
+			return $this;
+		}
+	}
+}
--- core/helpers/util.php
+++ core/helpers/util.php
@@ -16,6 +16,20 @@ if ( ! function_exists( 'wponion_timer' ) ) {
 	}
 }
 
+if ( ! function_exists( 'wponion_icon' ) ) {
+	/**
+	 * Checks and returns icon html + load the required icon font.
+	 *
+	 * @param        $icon
+	 * @param string $xtra_attrs
+	 *
+	 * @return string
+	 */
+	function wponion_icon( $icon, $xtra_attrs = '' ) {
+		return ( ! empty( $icon ) ) ? '<i class="' . $icon . ' wponion-icon" ' . $xtra_attrs . '> </i>' : '';
+	}
+}
+
 if ( ! function_exists( 'wponion_tooltip' ) ) {
 	/**
 	 * @param bool  $content
@@ -23,67 +37,31 @@ if ( ! function_exists( 'wponion_tooltip' ) ) {
 	 * @param bool  $element
 	 * @param bool  $localize
 	 *
-	 * @return array|string
+	 * @return array|string||\WPOnion\Util
 	 */
 	function wponion_tooltip( $content = false, $args = array(), $element = false, $localize = true ) {
 		if ( is_array( $content ) && ! empty( $args ) && is_string( $args ) ) {
 			$element = $args;
 			$args    = $content;
-			$content = null;
+			$content = ( isset( $args['content'] ) ) ? $args['content'] : null;
 		} elseif ( is_array( $content ) && empty( $args ) ) {
 			$args    = $content;
-			$content = null;
+			$content = ( isset( $args['content'] ) ) ? $args['content'] : null;
+			$element = ( isset( $args['element'] ) ) ? $args['element'] : null;
+		} elseif ( is_string( $content ) && is_array( $args ) ) {
+			if ( isset( $args['element'] ) ) {
+				$element = $args['element'];
+			}
 		} elseif ( is_string( $content ) && is_string( $args ) ) {
 			$element = $args;
 			$args    = array();
 		}
 
-
-		$args        = wp_parse_args( $args, array(
-			'content'     => $content,
-			'image'       => false,
-			'arrow'       => true,
-			'arrowType'   => 'round',
-			'js_field_id' => false,
-			'element'     => $element,
-			'placement'   => 'top',
-		) );
-		$js_field_id = $args['js_field_id'];
-		$element     = $args['element'];
-		unset( $args['js_field_id'] );
+		unset( $args['content'] );
 		unset( $args['element'] );
 
-		if ( false === $args['image'] && true === wponion_is_url( $args['content'] ) ) {
-			$args['image']   = $args['content'];
-			$args['content'] = false;
-		}
-
-		$attr = array( 'wponion-help' => 'wponion-help' );
-
-		if ( false !== $localize ) {
-			$localize = ( true === $localize ) ? 'wponion-help' : $localize;
-			$localize = ( false !== $element ) ? md5( $element ) : $localize;
-			if ( empty( $js_field_id ) ) {
-				$js_field_id = 'wpo' . wponion_hash_string( $element . microtime( true ) );
-			}
-			$attr['data-wponion-tooltip-id'] = $localize;
-			wponion_localize()->add( $js_field_id, array( $localize => $args ) );
-		} else {
-			$attr['data-tippy'] = $args['content'];
-		}
-
-		if ( false === $element ) {
-			return array(
-				'attr' => $attr,
-				'data' => $args,
-			);
-		}
-		$attr['data-wponion-jsid'] = $js_field_id;
-		preg_match_all( '/^(.?)<[a-z][a-z0-9]*?\b/s', $element, $matches, PREG_SET_ORDER, 0 );
-		if ( isset( $matches[0][0] ) ) {
-			return str_replace( $matches[0][0], $matches[0][0] . ' ' . wponion_array_to_html_attributes( $attr ) . ' ', $element );
-		}
-		return $element;
+		$instance = new \WPOnion\Util( $element, false );
+		return $instance->tooltip( $content, $args, $localize );
 	}
 }
 
@@ -111,52 +89,29 @@ if ( ! function_exists( 'wponion_tooltip_faq' ) ) {
 	}
 }
 
-
-if ( ! function_exists( 'wponion_icon' ) ) {
-	/**
-	 * Checks and returns icon html + load the required icon font.
-	 *
-	 * @param        $icon
-	 * @param string $xtra_attrs
-	 *
-	 * @return string
-	 */
-	function wponion_icon( $icon, $xtra_attrs = '' ) {
-		return ( ! empty( $icon ) ) ? '<i class="' . $icon . ' wponion-icon" ' . $xtra_attrs . '> </i>' : '';
-	}
-}
-
 if ( ! function_exists( 'wponion_inline_ajax' ) ) {
 	/**
 	 * @param array  $args
 	 * @param string $element
 	 *
-	 * @return string
+	 * @return string|\WPOnion\Util
 	 */
 	function wponion_inline_ajax( $args = array(), $element = '' ) {
-		$args = wp_parse_args( $args, array(
-			'method'      => 'post',
-			'url'         => admin_url( 'admin-ajax.php' ),
-			'part_url'    => false,
-			'data'        => array(),
-			'success'     => false,
-			'error'       => false,
-			'always'      => false,
-			'js_field_id' => false,
-			'element'     => $element,
-		) );
+		$instance = new \WPOnion\Util( $element, false );
+		return $instance->inline_ajax( $args );
+	}
+}
 
-		$element   = $args['element'];
-		$jsid      = $args['js_field_id'];
-		$jsid      = ( empty( $jsid ) ) ? 'wpo' . wponion_hash_string( wponion_hash_array( $args ) . microtime( true ) ) : $jsid;
-		$unique_id = 'wpoajax' . $jsid;
-		unset( $args['button'] );
-		unset( $args['js_field_id'] );
-		wponion_localize()->add( $unique_id, array( 'inline_ajax' => $args ) );
-		if ( ! empty( $element ) ) {
-			$element = preg_replace( '/<a (.+?)>/i', "<a $1 data-wponion-inline-ajax='" . $unique_id . "'>", $element );
-			return preg_replace( '/<button (.+?)>/i', "<button $1  data-wponion-inline-ajax='" . $unique_id . "'>", $element );
-		}
-		return $unique_id;
+if ( ! function_exists( 'wponion_image_popup' ) ) {
+	/**
+	 * @param string      $image_src
+	 * @param bool|string $full_size
+	 * @param bool|string $element
+	 *
+	 * @return string|bool|\WPOnion\Util
+	 */
+	function wponion_image_popup( $image_src = '', $full_size = false, $element = false ) {
+		$instance = new \WPOnion\Util( $element );
+		return $instance->image_popup( $image_src, $full_size, $element );
 	}
 }
