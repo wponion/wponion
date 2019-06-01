ed1c4a084d4c8934d18e3d8cbc564a2715fbfa1a
Varun <varunsridharan23@gmail.com>
2019-06-01 15:10:16 +0530

    Updated Sysinfo

--- core/wp/sysinfo/class-sysinfo.php
+++ core/wp/sysinfo/class-sysinfo.php
@@ -75,7 +75,7 @@ JAVASCRIPT;
 			$_content .= __( ' The system information shown below can also be copied and pasted into support requests such as on the WordPress.org forums, or to your theme and plugin developers. ', 'wponion' );
 			$_content .= '</p>';
 			$_content .= '<div id="sysreport" style="display:none;" ><textarea style="width:100%;min-height:250px;"  >';
-			$_content .= self::render_text_data( $data, $args );
+			$_content .= self::render_text_data( $data );
 			$_content .= '</textarea></div>';
 			$_content .= '<p>' . __( ' Some information may be filtered out from the list you are about to copy, this is information that may be considered private, and is not meant to be shared in a public forum. ', 'wponion' ) . '</p>';
 			$_content .= '<a href="javascript:void(0);" data-another-text="' . __( 'Copy Report', 'wponion' ) . '" class="button button-primary wponion-debug-report">' . __( 'Get system report', 'wponion' ) . '</a>';
@@ -145,6 +145,10 @@ JAVASCRIPT;
 					'html'  => $html,
 				) );
 			}
+
+			$ins = new \WPO\Fields\Content( self::render_text_data( $data ), true );
+			echo $ins->render();
+			return;
 
 			foreach ( $data as $key => $_data ) {
 				if ( isset( $args[ $key ] ) && false === $args[ $key ] || isset( $args[ $key . '_html' ] ) && false === $args[ $key . '_html' ] ) {
@@ -240,75 +244,102 @@ JAVASCRIPT;
 			return $data;
 		}
 
-		/**
-		 * @param $data
-		 * @param $args
-		 *
-		 * @return string
-		 * @static
-		 */
-		protected static function render_text_data( $data, $args ) {
-			$return = PHP_EOL;
-			foreach ( $data as $key => $_data ) {
-				if ( isset( $args[ $key ] ) && false === $args[ $key ] || isset( $args[ $key . '_text' ] ) && false === $args[ $key . '_text' ] ) {
-					continue;
+		protected static function eol( $data, $times = 2 ) {
+			return $data . str_repeat( PHP_EOL, $times );
+		}
+
+		public static function render_text_data( $data ) {
+			$return = '';
+			foreach ( $data as $key => $value ) {
+				if ( is_numeric( $key ) ) {
+				} else {
+					$return .= self::eol( '## ' . $key . ' ##', 1 );
+					$return .= ( is_array( $value ) ) ? self::render_text_array( $value, 2 ) : self::render_text_string( $value );
 				}
-				$return .= '### ' . $key . ' ###';
-				$return .= self::render_text_table( $_data );
+				$return .= self::eol( '' );
 			}
 			return $return;
 		}
 
-		/**
-		 * @param $data
-		 *
-		 * @return string
-		 * @static
-		 */
-		protected static function render_text_table( $data ) {
-			$return = PHP_EOL;
-
-			foreach ( $data as $key => $val ) {
-				if ( wponion_is_array( $val ) ) {
-					if ( isset( $val[0] ) ) {
-						$return .= $key . ' : ' . wp_json_encode( $val ) . PHP_EOL;
+		protected static function render_text_array( $value, $times ) {
+			$return = '';// self::eol( '', $times );
+			if ( ! empty( $value ) && is_array( $value ) ) {
+				foreach ( $value as $key => $val ) {
+					if ( is_numeric( $key ) ) {
+						if ( is_array( $val ) && isset( $val[0] ) ) {
+							$return .= implode( ',', $val );
+						} elseif ( is_array( $val ) && ! isset( $val[0] ) ) {
+							$return .= self::eol( self::render_text_array( $val, $times ), $times );
+						} else {
+							$val    = ( is_array( $val ) ) ? json_encode( $val ) : self::render_text_string( $val, $times );
+							$return .= self::eol( '* ' . $val, 1 );
+						}
 					} else {
-						$return .= '#### ' . $key . ': ####' . PHP_EOL;
-						foreach ( $val as $k => $v ) {
-							$v = ( wponion_is_array( $v ) ) ? wp_json_encode( $v ) : $v;
-
-							$return .= $k . ' : ' . self::render_text_bool( $v ) . PHP_EOL;
+						if ( is_array( $val ) && ! isset( $val[0] ) ) {
+							$return .= self::eol( '<details>', $times );
+							$return .= self::eol( '<summary>' . $key . ' </summary>', $times );
+							$return .= self::eol( '``` ', 1 );
+							$return .= self::eol( self::render_text_array( $val, 1 ) . '```', $times );
+							$return .= '</details>' . self::eol( '' );
+						} else {
+							if ( is_array( $val ) && isset( $val[0] ) && is_array( $val[0] ) ) {
+								$return .= self::eol( '### ' . $key . ' ###', $times );
+								$return .= self::render_text_array( $val, $times );
+							} elseif ( is_array( $val ) ) {
+								$return .= self::eol( '<details><summary>' . $key . ' : </summary> ', $times );
+								$return .= self::render_text_array( $val, $times ) . ' </details>';
+							} else {
+								$return .= $key . ' : ' . self::render_text_string( $val, $times );
+							}
 						}
 					}
-				} else {
-					$return .= $key . ' : ' . self::render_text_bool( $val ) . PHP_EOL;
 				}
 			}
-			return $return . PHP_EOL;
+			return $return;
+		}
+
+		protected static function render_text_string( $value, $times = 1 ) {
+			$value = self::render_text_bool( $value );
+			if ( ! is_numeric( $value ) ) {
+				json_decode( $value );
+
+				if ( JSON_ERROR_NONE === json_last_error() ) {
+					return '			
+```json
+' . $value . '
+```
+';
+				}
+			}
+			return self::eol( $value, $times );
 		}
 
 		/**
 		 * @param $data
 		 *
-		 * @return string
+		 * @return string|array
 		 * @static
 		 */
 		protected static function render_text_bool( $data ) {
+			if ( is_array( $data ) ) {
+				return $data;
+			}
+
 			if ( true === $data ) {
 				return '✔';
-			} elseif ( 'yes' === strtolower( $data ) ) {
-				return '✔';
-			} elseif ( 'on' === strtolower( $data ) ) {
+			}
+
+			if ( in_array( $data, array( 'yes', 'on', 1, '1' ), true ) ) {
 				return '✔';
 			}
+
 			if ( false === $data ) {
-				return '❌';
-			} elseif ( 'no' === strtolower( $data ) ) {
-				return '❌';
-			} elseif ( 'off' === strtolower( $data ) ) {
 				return '❌';
 			}
 
+			if ( in_array( $data, array( 'off', 'no', 0, '0' ), true ) ) {
+				return '✔';
+			}
 			return $data;
 		}
 
