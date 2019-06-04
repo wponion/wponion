49e514ce84cb249d09506779294fb9c8c743b72e
Varun <varunsridharan23@gmail.com>
2019-06-04 19:05:06 +0530

    Faq render Issue fixed.

--- fields/faq/class-faq.php
+++ fields/faq/class-faq.php
@@ -40,7 +40,7 @@ if ( ! class_exists( '\WPOnion\Field\FAQ' ) ) {
 				echo '<li class="faq">';
 				echo '<div class="faq-title"><h3><i class="dashicons"></i><span class="title-name">' . $faq['heading'] . '</span></h3></div>';
 				echo '<div class="faq-content">';
-				wpo_field( 'markdown', $faq['content'] )
+				echo wpo_field( 'markdown', $faq['content'] )
 					->only_field( true )
 					->render( null, null );
 				echo '</div>';
