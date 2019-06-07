44d3ca4f39b0a189abc56e13f9a3eb3cf6dd29ff
Varun <varunsridharan23@gmail.com>
2019-06-07 17:06:11 +0530

    Moved `wponion_update_option` and `wponion_get_option` to **db.php**

--- core/helpers/modules/settings.php
+++ core/helpers/modules/settings.php
@@ -58,39 +58,3 @@ if ( ! function_exists( 'wponion_network_settings' ) ) {
 		return new \WPOnion\Modules\Settings\Network( $instance_id_or_args, $fields );
 	}
 }
-
-if ( ! function_exists( 'wponion_update_option' ) ) {
-	/**
-	 * Custom Wrapper For update_option & update_site_option.
-	 *
-	 * @param string $option
-	 * @param mixed  $value
-	 * @param bool   $autoload
-	 * @param bool   $force_local
-	 */
-	function wponion_update_option( $option, $value, $autoload = false, $force_local = false ) {
-		if ( is_network_admin() && is_multisite() && false === $force_local ) {
-			update_site_option( $option, $value );
-		} else {
-			update_option( $option, $value, $autoload );
-		}
-	}
-}
-
-if ( ! function_exists( 'wponion_get_option' ) ) {
-	/**
-	 * Custom Wrapper for get_option / get_site_option.
-	 *
-	 * @param      $option_name
-	 * @param      $default
-	 * @param bool $force_local
-	 *
-	 * @return mixed
-	 */
-	function wponion_get_option( $option_name, $default, $force_local = false ) {
-		if ( is_multisite() && is_network_admin() && false === $force_local ) {
-			return get_site_option( $option_name, $default );
-		}
-		return get_option( $option_name, $default );
-	}
-}
