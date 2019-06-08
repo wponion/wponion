1ad50d744e54b31dee5dc6f1b4980dedf3b3681e
Varun <varunsridharan23@gmail.com>
2019-06-08 09:45:16 +0530

    Updated hook functions.

--- core/modules/settings/class-settings.php
+++ core/modules/settings/class-settings.php
@@ -129,7 +129,7 @@ if ( ! class_exists( '\WPOnion\Modules\Settings' ) ) {
 		 * Loads Required Style for the current settings page.
 		 */
 		public function load_admin_styles() {
-			$this->_action( 'page_assets' );
+			do_action( 'wponion_settings_page_assets', $this->unique() );
 		}
 
 		/**
@@ -160,7 +160,7 @@ if ( ! class_exists( '\WPOnion\Modules\Settings' ) ) {
 							}
 						}
 					}
-					$this->_action( 'register_submenu', $menu['menu_slug'], $this );
+					do_action( 'wponion_settings_register_submenu', $menu['menu_slug'], $this->unique(), $this );
 				}
 			}
 		}
@@ -308,7 +308,7 @@ if ( ! class_exists( '\WPOnion\Modules\Settings' ) ) {
 		public function on_settings_page_load() {
 			$this->find_active_menu();
 			$this->settings_menus();
-			$this->add_action( 'admin_enqueue_scripts', 'load_admin_styles' );
+			//$this->add_action( 'admin_enqueue_scripts', 'load_admin_styles' );
 			$user_menu = $this->option( 'menu' );
 			if ( isset( $user_menu['submenu'] ) && ( true === $user_menu['submenu'] || wponion_is_array( $user_menu['submenu'] ) ) ) {
 				global $submenu_file;
@@ -319,7 +319,7 @@ if ( ! class_exists( '\WPOnion\Modules\Settings' ) ) {
 			}
 
 			$this->init_theme();
-			$this->_action( 'page_onload' );
+			do_action( 'wponion_settings_page_onload', $this->unique() );
 		}
 
 		/**
