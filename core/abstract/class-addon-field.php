27f28d42af370e598b23f310bb0e7609f3e24386
Varun <varunsridharan23@gmail.com>
2019-06-08 09:40:48 +0530

    Updated naming convention

--- core/abstract/class-addon-field.php
+++ core/abstract/class-addon-field.php
@@ -28,19 +28,19 @@ if ( ! class_exists( '\WPOnion\Addon_Field' ) ) {
 		 * @var null
 		 * @access
 		 */
-		protected $field_type = null;
+		protected $type = null;
 
 		/**
 		 * @var null
 		 * @access
 		 */
-		protected $field_supports = null;
+		protected $supports = null;
 
 		/**
 		 * @var array
 		 * @access
 		 */
-		protected $field_args = array();
+		protected $args = array();
 
 		/**
 		 * Stores Field Class File Location.
@@ -48,17 +48,17 @@ if ( ! class_exists( '\WPOnion\Addon_Field' ) ) {
 		 * @var null
 		 * @access
 		 */
-		protected $field_class_file = null;
+		protected $class_file = null;
 
 		/**
 		 * Addon_Field constructor.
 		 *
-		 * @param bool   $addon_name
-		 * @param string $addon_file
+		 * @param bool   $name
+		 * @param string $file
 		 * @param null   $version
 		 */
-		public function __construct( $addon_name = false, $addon_file = __FILE__, $version = null ) {
-			parent::__construct( $addon_name, $addon_file, $version );
+		public function __construct( $name = false, $file = __FILE__, $version = null ) {
+			parent::__construct( $name, $file, $version );
 			add_action( 'wponion_core_fields_registered', array( &$this, 'register_field' ) );
 			add_action( 'wponion_load_field_class', array( &$this, '_load_field_class' ), 10, 3 );
 		}
@@ -69,8 +69,8 @@ if ( ! class_exists( '\WPOnion\Addon_Field' ) ) {
 		 * @hook wponion_core_fields_registered
 		 */
 		public function register_field() {
-			if ( ! empty( $this->field_type ) ) {
-				wponion_register_field( $this->field_type, $this->field_supports, $this->field_args );
+			if ( ! empty( $this->type ) ) {
+				wponion_register_field( $this->type, $this->supports, $this->args );
 			}
 		}
 
@@ -82,12 +82,12 @@ if ( ! class_exists( '\WPOnion\Addon_Field' ) ) {
 		 * @param $module
 		 */
 		public function _load_field_class( $class, $field_type, $module ) {
-			if ( strtolower( $field_type ) !== strtolower( $this->field_type ) ) {
+			if ( strtolower( $field_type ) !== strtolower( $this->type ) ) {
 				return;
 			}
 
 			if ( ! empty( $this->field_class_file ) ) {
-				require_once $this->dir( $this->field_class_file );
+				require_once $this->dir( $this->class_file );
 			} else {
 				$this->load_field_class( $class, $field_type, $module );
 			}
