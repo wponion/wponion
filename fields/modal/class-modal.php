5c6a95823f4847386c7e547f01939005e2459ad4
Varun <varunsridharan23@gmail.com>
2019-06-13 12:51:42 +0530

    modal field work going on.

---
+++ fields/modal/class-modal.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ *
+ * Initial version created 12-05-2018 / 07:05 PM
+ *
+ * @author Varun Sridharan <varunsridharan23@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * @package
+ * @link
+ * @copyright 2018 Varun Sridharan
+ * @license GPLV3 Or Greater (https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
+namespace WPOnion\Field;
+
+use WPOnion\Field;
+use WPOnion\Modules\Metabox\Core;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+if ( ! class_exists( '\WPOnion\Field\Modal' ) ) {
+	/**
+	 * Class Modal
+	 *
+	 * @package WPOnion\Field
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class Modal extends Field {
+		/**
+		 * Final HTML Output
+		 */
+		public function output() {
+			echo $this->before();
+
+			if ( ! is_ajax() ) {
+				$btn = $this->handle_args( 'label', $this->data( 'button' ), array( 'class' => 'button button-secondary' ), array(
+					'type'       => 'button',
+					'only_field' => true,
+				) );
+				echo $this->sub_field( $btn, null, null );
+				echo '<div class="wponion-modal-hidden-data"></div>';
+			}
+			if ( is_ajax() ) {
+				$output = '<div class=" wponion-modal-html wponion-modal-field wponion-on-modal">';
+				$this->catch_output( 'start' );
+				if ( ! empty( $this->data( 'fields' ) ) && is_array( $this->data( 'fields' ) ) ) {
+					foreach ( $this->data( 'fields' ) as $field ) {
+						echo $this->sub_field( $field, wponion_get_field_value( $field, $this->value() ), $this->name() );
+					}
+				}
+				$html   = $this->catch_output( 'stop' );
+				$output .= $html . '</div>';
+
+				echo $output;
+			}
+			echo $this->after();
+		}
+
+		public function field_assets() {
+		}
+
+		/**
+		 * @return array
+		 */
+		protected function js_field_args() {
+			return array(
+				'modal_type'   => $this->data( 'modal_type' ),
+				'modal_config' => $this->data( 'modal_config' ),
+				'modal_html'   => $this->field['modal_html'],
+			);
+		}
+
+		/**
+		 * Final HTML Output;
+		 *
+		 * @return mixed;
+		 */
+		protected function field_default() {
+			return array(
+				'fields'       => array(),
+				'modal_config' => array(),
+				'modal_type'   => 'swal', // SWAL / WP.
+				'button'       => __( 'Open Modal' ),
+			);
+		}
+	}
+}
--- src/js/fields/modal.js
+++ src/js/fields/modal.js
@@ -18,16 +18,40 @@ class field extends WPOnion_Field {
 
 	init_sweatalert() {
 		let $args          = this.option( 'modal_config' );
-		//$args.html         = this.option( 'modal_html' );
-		let $modal         = this.element.find( '>  .row > .wponion-fieldset > .wponion-modal-html' );
-		$args.html         = '<div class="wponion-framework">' + this.option( 'modal_html' ) + '</div>';
 		$args.onBeforeOpen = () => {
-			let $mainelem = this.element.find( '#swal2-content .wponion-framework' );
-			wponion_field( $mainelem ).reload();
-			/*$mainelem.on( 'blur change click dblclick error keydown keypress keyup select', () => {
-				console.log( $mainelem.find( ':input' ).serializeArray() );
-				console.log( $mainelem.html() );
-			} );*/
+			let $mainelem = this.element.find( '#swal2-content' );
+			let $hidden   = this.element.find( '.wponion-modal-hidden-data' );
+			swal.showLoading();
+			this.ajax( 'modal-popup-fields', {
+				data: {
+					unique: this.option( 'unique' ),
+					field_path: this.option( 'field_path' ),
+				},
+				onAlways: () => swal.hideLoading(),
+				onSuccess: ( res ) => {
+					$mainelem.html( res );
+					wponion_field( $mainelem ).reload();
+					$mainelem.show();
+				},
+				onError: () => {
+					swal.fire( {
+						type: 'error',
+						text: window.wponion.core.txt( 'unknown_ajax_error' ),
+					} );
+				},
+			} );
+
+			//wponion_field( $mainelem ).reload();
+			$mainelem.on( 'blur change click dblclick error keydown keypress keyup select', () => {
+				let $inputs = $mainelem.find( ':input' ).serializeArray();
+				$hidden.html( '' );
+				for( let $i in $inputs ) {
+					if( $inputs.hasOwnProperty( $i ) ) {
+						$hidden.append( '<input type="hidden" name="' + $inputs[ $i ].name + '" value="' + $inputs[ $i ].value + '">' );
+					}
+				}
+
+			} );
 		};
 		$args              = this.handle_args( $args, 'SweatAlert Modal' );
 		$args.target       = this.element[ 0 ];
