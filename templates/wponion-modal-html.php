a006fc72a7a18d49c33d55c1fa94cb5b338641e4
Varun <varunsridharan23@gmail.com>
2019-06-15 11:27:04 +0530

    template updated.

--- templates/wponion-modal-html.php
+++ templates/wponion-modal-html.php
@@ -1,27 +1,21 @@
 <?php
-$title = __( 'Close media panel', 'wponion' );
+$title = __( 'Close Panel', 'wponion' );
 return <<<HTML
-<div tabindex="0" class="media-modal wp-core-ui">
+<div tabindex="0" class="wponion-wp-modal media-modal wp-core-ui">
 
 	<button type="button" class="media-modal-close">
-	<span class="media-modal-icon">
-		<span class="screen-reader-text">{$title}</span>
-	</span>
+		<span class="media-modal-icon"><span class="screen-reader-text">{$title}</span></span>
 	</button>
 
 	<div class="media-modal-content">
 		<div class="media-frame">
-			<div class="media-frame-menu">
-				<div class="media-menu"></div>
-			</div>
+			<div class="media-frame-menu"><div class="media-menu"></div></div>
 			<div class="wponion-modal-content-container"></div>
-
 			<div class="media-frame-toolbar">
 				<div class="media-toolbar">
 					<div class="media-toolbar-secondary"></div>
 					<div class="media-toolbar-primary search-form">
-						<button type="button"
-								class="button media-button button-primary button-large media-button-select">Select
+						<button type="button" class="button media-button button-primary button-large media-button-select">Select
 						</button>
 					</div>
 				</div>
@@ -31,6 +25,6 @@ return <<<HTML
 	</div>
 </div>
 
-<div class="media-modal-backdrop"></div>
+<div class="wponion-wp-modal-backdrop media-modal-backdrop"></div>
 HTML;
 
