d32cf84b8011c741b87a335a3c77bfbc80db0c1e
Varun <varunsridharan23@gmail.com>
2019-06-15 11:25:57 +0530

    Work On Progress.

--- fields/modal/class-modal.php
+++ fields/modal/class-modal.php
@@ -35,32 +35,43 @@ if ( ! class_exists( '\WPOnion\Field\Modal' ) ) {
 		 */
 		public function output() {
 			echo $this->before();
-
-			if ( ! is_ajax() ) {
-				$btn = $this->handle_args( 'label', $this->data( 'button' ), array( 'class' => 'button button-secondary' ), array(
-					'type'       => 'button',
-					'only_field' => true,
-				) );
-				echo $this->sub_field( $btn, null, null );
-				echo '<div class="wponion-modal-hidden-data"></div>';
+			$btn = $this->handle_args( 'label', $this->data( 'button' ), array( 'class' => 'button button-secondary' ), array(
+				'type'       => 'button',
+				'only_field' => true,
+			) );
+			echo $this->sub_field( $btn, null, null );
+			echo '<div class="wponion-modal-hidden-data"></div>';
+			if ( 'swal' === $this->data( 'modal_type' ) ) {
+				$this->swal();
+			} else {
+				$this->wp();
 			}
-			if ( is_ajax() ) {
-				$output = '<div class=" wponion-modal-html wponion-modal-field wponion-on-modal">';
-				$this->catch_output( 'start' );
-				if ( ! empty( $this->data( 'fields' ) ) && is_array( $this->data( 'fields' ) ) ) {
-					foreach ( $this->data( 'fields' ) as $field ) {
-						echo $this->sub_field( $field, wponion_get_field_value( $field, $this->value() ), $this->name() );
-					}
-				}
-				$html   = $this->catch_output( 'stop' );
-				$output .= $html . '</div>';
 
-				echo $output;
-			}
 			echo $this->after();
 		}
 
+		public function swal() {
+			$output = '<div class=" wponion-modal-html wponion-modal-field wponion-on-modal">';
+			$this->catch_output( 'start' );
+			if ( ! empty( $this->data( 'fields' ) ) && is_array( $this->data( 'fields' ) ) ) {
+				foreach ( $this->data( 'fields' ) as $field ) {
+					echo $this->sub_field( $field, wponion_get_field_value( $field, $this->value() ), $this->name() );
+				}
+			}
+			$html   = $this->catch_output( 'stop' );
+			$output .= $html . '</div>';
+
+			echo $output;
+		}
+
+		public function wp() {
+
+		}
+
 		public function field_assets() {
+			wp_enqueue_script( 'underscore' );
+			wp_enqueue_media();
+			wp_enqueue_style( 'media-views' );
 		}
 
 		/**
@@ -83,7 +94,7 @@ if ( ! class_exists( '\WPOnion\Field\Modal' ) ) {
 			return array(
 				'fields'       => array(),
 				'modal_config' => array(),
-				'modal_type'   => 'swal', // SWAL / WP.
+				'modal_type'   => 'wp', // SWAL / WP.
 				'button'       => __( 'Open Modal' ),
 			);
 		}
