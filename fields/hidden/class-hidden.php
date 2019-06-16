c639bf031b061111f7e398dd2a216a3dfbf96e98
Varun <varunsridharan23@gmail.com>
2019-06-16 07:07:49 +0530

    Updated hidden filed to handle nested fields.

--- fields/hidden/class-hidden.php
+++ fields/hidden/class-hidden.php
@@ -15,28 +15,39 @@ if ( ! class_exists( '\WPOnion\Field\Hidden' ) ) {
 	 * @since 1.0
 	 */
 	class Hidden extends \WPOnion\Field {
-
+		/**
+		 * @param array $data
+		 *
+		 * @return array
+		 */
 		public function handle_field_args( $data = array() ) {
 			$data['only_field'] = true;
 			return $data;
 		}
 
 		public function output() {
-			$attr = $this->attributes( array(
-				'type'              => 'hidden',
-				'class'             => $this->element_class(),
-				'value'             => $this->value(),
-				'name'              => $this->name(),
-				'data-wponion-jsid' => $this->js_field_id(),
-			) );
-			echo '<input type="hidden" name="' . $this->name() . '"  ' . $attr . '/>';
+			if ( ! empty( $this->data( 'fields' ) ) ) {
+				foreach ( $this->data( 'fields' ) as $field ) {
+					$field['type'] = 'hidden';
+					echo $this->sub_field( $field, wponion_get_field_value( $field, $this->value() ), $this->name() );
+				}
+			} else {
+				$attr = $this->attributes( array(
+					'type'              => 'hidden',
+					'class'             => $this->element_class(),
+					'value'             => $this->value(),
+					'name'              => $this->name(),
+					'data-wponion-jsid' => $this->js_field_id(),
+				) );
+				echo '<input type="hidden" name="' . $this->name() . '"  ' . $attr . '/>';
+			}
 		}
 
 		public function field_assets() {
 		}
 
 		protected function field_default() {
-			return array();
+			return array( 'fields' => array() );
 		}
 	}
 }
