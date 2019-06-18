3ae3cf11561e33331b50008917ffb86943caced0
Varun <varunsridharan23@gmail.com>
2019-06-18 10:46:41 +0530

    updated settings module to use new script argument.

--- src/js/modules/settings.js
+++ src/js/modules/settings.js
@@ -22,7 +22,7 @@ class WPOnion_Settings_Module extends WPOnion_Module {
 					element_lock: jQuery( 'button.wponion-save' ),
 					success: ( response ) => {
 						let $elem = jQuery( response.form );
-						jQuery( 'body' ).find( 'script#wponion_field_js_vars' ).append( response.script );
+						window.wponion.core.script_tag( response.script );
 						this.element.parent().html( $elem.find( '.wponion-form' ).html() );
 						let $settings = window.wponion._.clone( window.wponion.core.option( 'settings_ajax' ) );
 
