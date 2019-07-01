4d17b963ff9d3e99fc495d89d95a5bfa64b95ad8
Varun <varunsridharan23@gmail.com>
2019-07-01 09:30:29 +0530

    Updated Version To 1.3

--- wponion-init.php
+++ wponion-init.php
@@ -12,7 +12,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-defined( 'WPONION_VERSION' ) or define( 'WPONION_VERSION', '1.2.1' );
+defined( 'WPONION_VERSION' ) or define( 'WPONION_VERSION', '1.3' );
 defined( 'WPONION_CDN_VERSION' ) or define( 'WPONION_CDN_VERSION', '1.2' );
 defined( 'WPONION_CDN_URL' ) or define( 'WPONION_CDN_URL', 'https://cdn.jsdelivr.net/gh/wponion/vendor-support' );
 defined( 'WPONION_NAME' ) or define( 'WPONION_NAME', 'WPOnion' );
