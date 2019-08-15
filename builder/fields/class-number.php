6c2af23c3e5dcfd8c31e4e45bda3d9a3cd11c48e
Varun <varunsridharan23@gmail.com>
2019-08-15 10:11:12 +0530

    added number field.

---
+++ builder/fields/class-number.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace WPO\Fields;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+if ( ! class_exists( 'WPO\Fields\Number' ) ) {
+	/**
+	 * Class Number
+	 *
+	 * @package WPO\Fields
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class Number extends Text {
+		/**
+		 * Text constructor.
+		 *
+		 * @param bool  $id
+		 * @param bool  $title
+		 * @param array $args
+		 */
+		public function __construct( $id = false, $title = false, $args = array() ) {
+			parent::__construct( $id, $title, $args );
+			$this['type'] = 'number';
+		}
+
+		/**
+		 * @param $min
+		 *
+		 * @return $this
+		 */
+		public function min( $min ) {
+			$this['min'] = $min;
+			return $this;
+		}
+
+		/**
+		 * @param $min
+		 *
+		 * @return $this
+		 */
+		public function max( $max ) {
+			$this['max'] = $max;
+			return $this;
+		}
+
+		/**
+		 * @param $min
+		 *
+		 * @return $this
+		 */
+		public function step( $step ) {
+			$this['step'] = $step;
+			return $this;
+		}
+	}
+}
