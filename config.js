6b7eed8432f36cc10601c06b6ebb541e56ee4560
Varun <varunsridharan23@gmail.com>
2020-03-26 10:11:12 +0530

    Moved WPOnion Icon to a package system.

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ $files[ 'src/scss/wponion-base.scss' ]    = {
 		'src/scss/themes/*/*',
 		'src/scss/wponion-base.scss',
 		'src/scss/wponion-utility.scss',
+		'src/scss/wponion-icon-fonts.scss',
 		'src/scss/includes/*',
 		'src/scss/includes/fields/*',
 		'src/scss/includes/bootstrap/*',
