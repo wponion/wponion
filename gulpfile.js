89387a5ca6b7856e237ac75fb80697ef1f965994
Varun <varunsridharan23@gmail.com>
2020-03-26 10:23:01 +0530

    Added Gulp Copy Task to copy latest font files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
-let $a      = require( 'easy-gulp-tasker' );
+require( 'easy-gulp-tasker' );
+let $gulp        = require( 'gulp' ),
+	$assets_path = './assets/',
+	$filesToCopy = {
+		'./node_modules/@wponion/icons/fonts/*.*': 'font/',
+	};
+
+$gulp.task( 'copy', ( callback ) => {
+	let $x;
+	for( $x in $filesToCopy ) {
+		if( $filesToCopy.hasOwnProperty( $x ) ) {
+			let $dist_path = $filesToCopy[ $x ];
+			$gulp.src( $x ).pipe( $gulp.dest( $assets_path + $dist_path ) );
+		}
+	}
+	callback();
+} );
