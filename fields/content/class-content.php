1ca72e89be592ab902adb3301af8c7154a9d70f0
Varun <varunsridharan23@gmail.com>
2020-05-04 21:41:05 +0530

    #234 Fixed.

--- fields/content/class-content.php
+++ fields/content/class-content.php
@@ -27,16 +27,14 @@ if ( ! class_exists( '\WPOnion\Field\Content' ) ) {
 			echo $this->before();
 			$content = $this->data( 'content' );
 
-			if ( ! empty( $this->data( 'content' ) ) ) {
-				if ( wponion_is_callable( $this->data( 'content' ) ) ) {
-					$this->catch_output( 'start' );
-					echo wponion_callback( $this->data( 'content' ) );
-					$content = $this->catch_output( 'end' );
-				} elseif ( file_exists( $this->data( 'content' ) ) ) {
-					$this->catch_output( 'start' );
-					include $this->data( 'content' );
-					$content = $this->catch_output( 'end' );
-				}
+			if ( ! empty( $this->data( 'content_path' ) ) && file_exists( $this->data( 'content_path' ) ) ) {
+				$this->catch_output( 'start' );
+				include $this->data( 'content_path' );
+				$content = $this->catch_output( 'end' );
+			} elseif ( ! empty( $this->data( 'content' ) ) && wponion_is_callable( $this->data( 'content' ) ) ) {
+				$this->catch_output( 'start' );
+				echo wponion_callback( $this->data( 'content' ) );
+				$content = $this->catch_output( 'end' );
 			}
 
 			if ( $this->has( 'markdown' ) && true === $this->has( 'markdown' ) && ! empty( $content ) ) {
@@ -55,8 +53,9 @@ if ( ! class_exists( '\WPOnion\Field\Content' ) ) {
 		 */
 		protected function field_default() {
 			return array(
-				'content'  => false,
-				'markdown' => false,
+				'content'      => false,
+				'content_path' => false,
+				'markdown'     => false,
 			);
 		}
 
