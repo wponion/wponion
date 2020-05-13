9164283298c550b5cb7ad9f5afcbebf8d672941f
Varun <varunsridharan23@gmail.com>
2020-05-13 14:25:30 +0530

    * removed `handle_data`

--- core/abstract/abstract-bridge.php
+++ core/abstract/abstract-bridge.php
@@ -5,9 +5,7 @@ namespace WPOnion;
 use WPOnion\Traits\Class_Options;
 use WPOnion\Traits\Hooks;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( '\WPOnion\Bridge' ) ) {
 	/**
@@ -19,34 +17,5 @@ if ( ! class_exists( '\WPOnion\Bridge' ) ) {
 	abstract class Bridge {
 		use Class_Options;
 		use Hooks;
-
-		/**
-		 * Returns a default array.
-		 *
-		 * @return array
-		 */
-		protected function defaults() {
-			return array();
-		}
-
-		/**
-		 * Checks And handles data.
-		 *
-		 * @param array  $data
-		 * @param array  $defaults
-		 * @param string $save_with
-		 *
-		 * @return array
-		 */
-		protected function handle_data( $data = array(), $defaults = array(), $save_with = '' ) {
-			if ( true === $data ) {
-				return $data;
-			} elseif ( is_array( $data ) ) {
-				return $this->parse_args( $data, $defaults );
-			} elseif ( false !== $data && isset( $defaults[ $save_with ] ) ) {
-				$defaults[ $save_with ] = $data;
-			}
-			return $defaults;
-		}
 	}
 }
