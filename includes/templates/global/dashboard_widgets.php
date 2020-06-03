7818e904e27403c9f6b084973e0b63d53307ce9a
Varun <varunsridharan23@gmail.com>
2020-06-03 08:11:26 +0530

    Updated Dashboard Widgets Template.

--- includes/templates/global/dashboard_widgets.php
+++ includes/templates/global/dashboard_widgets.php
@@ -1,17 +1,13 @@
 <?php
 defined( 'ABSPATH' ) || exit;
 
-/**
- * @var \WPOnion\Theme\WP_Modern $this
- */
+/* @var \WPOnion\Theme\WP_Modern $this */
 $dashboard_widgets = $this->module_instance();
-$fields            = $dashboard_widgets->fields();
-?>
-
-<div <?php echo $dashboard_widgets->wrap_attributes(); ?>>
-	<?php
-	foreach ( $fields->fields() as $field ) {
+$fields            = $dashboard_widgets->fields()->get();
+if ( ! empty( $fields ) ) {
+	echo "<div {$dashboard_widgets->wrap_attributes()}>";
+	foreach ( $fields as $field ) {
 		echo $dashboard_widgets->render_field( $field );
 	}
-	?>
-</div>
+	echo '</div>';
+}
--- includes/templates/wp-modern/dashboard_widgets.php
+++ includes/templates/wp-modern/dashboard_widgets.php
@@ -1,15 +1,2 @@
 <?php
-defined( 'ABSPATH' ) || exit;
-
-/* @var \WPOnion\Theme\WP_Modern $this */
-$dashboard_widgets = $this->module_instance();
-$fields            = $dashboard_widgets->fields();
-?>
-
-<div <?php echo $dashboard_widgets->wrap_attributes(); ?>>
-	<?php
-	foreach ( $fields->fields() as $field ) {
-		echo $dashboard_widgets->render_field( $field );
-	}
-	?>
-</div>
+include wponion()->tpl( 'global/dashboard_widgets.php' );
