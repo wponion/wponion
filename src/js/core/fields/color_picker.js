2d578c366948e8096bd80f672a2c6580bef06d5b
Varun Sridharan <varunsridharan23@gmail.com>
2021-03-06 18:14:32 +0530

    Improved Color Picker Field.

--- src/js/core/fields/color_picker.js
+++ src/js/core/fields/color_picker.js
@@ -8,30 +8,36 @@ export default class extends WPOnion_Field {
 		if( this.element.find( 'input.wponion-color-picker-element' ).length > 0 ) {
 			let $data       = this.option( 'settings', {} );
 			$data           = ( !window.wponion._.isObject( $data ) ) ? {} : $data;
-			let $save_color = ( $color, $instance ) => {
-					let $value = $color.toHEXA().toString();
-					if( !window.wponion._.isUndefined( $instance._representation ) ) {
-						switch( $instance._representation ) {
-							case 'HEXA':
-								$value = $color.toHEXA().toString();
-								break;
-							case 'RGBA':
-								$value = $color.toRGBA().toString();
-								break;
-							case 'HSLA':
-								$value = $color.toHSLA().toString();
-								break;
-							case 'HSVA':
-								$value = $color.toHSVA().toString();
-								break;
-							case 'CMYK':
-								$value = $color.toCMYK().toString();
-								break;
+			let $save_color = ( $color ) => {
+					let $value = '';
+					let $display = '';
+					if( !window.wponion._.isNull( $color ) ) {
+						$value = $color.toHEXA().toString();
+						$display = $color.toHEXA().toString();
+						if( !window.wponion._.isUndefined( $instance._representation ) ) {
+							switch( $instance._representation ) {
+								case 'HEXA':
+									$value = $color.toHEXA().toString();
+									break;
+								case 'RGBA':
+									$value = $color.toRGBA().toString();
+									break;
+								case 'HSLA':
+									$value = $color.toHSLA().toString();
+									break;
+								case 'HSVA':
+									$value = $color.toHSVA().toString();
+									break;
+								case 'CMYK':
+									$value = $color.toCMYK().toString();
+									break;
+							}
 						}
 					}
+
 					this.element.find( 'input.wponion-color-picker-element' ).val( $value );
 					this.element.find( 'input.wponion-color-picker-element' ).trigger( 'change' );
-					this.element.find( 'span.cpickr-bg' ).css( 'background-color', $value );
+					this.element.find( 'span.cpickr-bg' ).css( 'background-color', $display );
 				},
 				$args       = this.parse_args( $data, {
 					theme: 'classic',
@@ -53,20 +59,24 @@ export default class extends WPOnion_Field {
 					}
 				} );
 
-			$args.appClass = 'wpo-color-picker';
-			$args.default  = this.element.find( 'input.wponion-color-picker-element' ).val() || '#fff';
-			$args          = this.handle_args( $args, 'colorpicker' );
-			$args.el       = this.element.find( 'div.wponion-color-picker-element' )[ 0 ];
-			let $instance  = new Pickr( $args ),
-				$input     = this.element.find( 'input.wponion-color-picker-element' );
+			let $is_inline     = $args.inline;
+			let $is_show_input = $args.components.interaction.input;
+			$args.appClass     = 'wpo-color-picker';
+			$args.default      = this.element.find( 'input.wponion-color-picker-element' ).val() || '#fff';
+			$args              = this.handle_args( $args, 'colorpicker' );
+			$args.el           = this.element.find( 'div.wponion-color-picker-element' )[ 0 ];
+			let $instance      = new Pickr( $args ),
+				$input         = this.element.find( 'input.wponion-color-picker-element' );
 
 			$instance.on( 'save', $save_color );
 			$instance.on( 'change', $save_color );
 			$instance.on( 'swatchselect', $save_color );
 			$instance.on( 'init', () => {
-				if( !window.wponion._.isUndefined( $args.inline ) && true === $args.inline ) {
-					this.element.find( '.wponion-color-picker-element' ).hide();
-					this.element.find( '.pickr' ).hide();
+				if( true === $is_inline ) {
+					if( true !== $is_show_input ) {
+						this.element.find( '.wponion-color-picker-element' ).hide();
+						this.element.find( '.pickr' ).hide();
+					}
 				}
 			} );
 
